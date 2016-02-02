4fc23b11dbb0a80e0bd6e14e99155d036bc794a3
Faiyaz Ahmed <ahmedf@vmware.com>
2016-02-02 10:29:41 -0800

    Add build.sh to build from Docker or locally
    build-iso.sh is called by both the Dockerfile (when building in a
    container) and by build.sh directly when building in situ.
    
    The Dockerfile does nothing more than call build-iso.sh after resolving
    some iso and build dependencies.
    
    All bootstrap dependencies are resolved locally (in build.sh) once and
    reused for either local or docker based builds.
    
    build-iso.sh is idempotent with respect to the directories it uses to
    create the iso, unpack the kernel, and rebuild the initrd.

--- bootstrap/build.sh
+++ bootstrap/build.sh
@@ -1,80 +1,104 @@
 #!/bin/bash
 
+#
+# Build a booststrap iso bootable by VIC.
+#
+
 [ -n "$DEBUG" ] && set -x
 
-NAME=container
+set -e
 
-# set default SRCDIR & BINDIR for local builds
-if [ "${SRCDIR}" == "" ]; then
-  SRCDIR="$(cd `dirname "$0"` && pwd)"
+function usage () {
+     echo "$0 { docker | local }"
+     exit 1
+}
+
+if [ $# -lt 1 ]; then
+     usage
 fi
 
-if [ "${BINDIR}" == "" ]; then
-  BINDIR=${SRCDIR}/../../binary
+BINARY=${BINARY:-../binary}
+name=bootstrap
+
+git_args=(--git-dir=../.git --work-tree=../)
+branch_name=$(git "${git_args[@]}" symbolic-ref HEAD 2>/dev/null)
+branch=${branch_name##refs/heads/}
+sha=$(git "${git_args[@]}" rev-parse --short HEAD)
+date=$(date -u +%Y/%m/%d_@_%H:%M:%S)
+BUILD_ID="$date@$branch:$sha"
+
+# The iso being created
+isofile=${ISOFILE:-../binary/bootstrap-${sha}.iso}
+
+# The vendor directory.
+vendor=${VENDOR:-../vendor}
+
+# Grab the kernel
+PHOTON_RPM_REPO=${PHOTON_RPM_REPO:-http://bonneville.eng.vmware.com:8080/job/bonneville-kernel/lastSuccessfulBuild/artifact/binary/}
+photon_kernel_rpm=linux-esx-4.2.0-10.x86_64.rpm
+
+if [ ! -d "$BINARY" ]; then
+     mkdir "$BINARY"
 fi
-BINBASE=$(basename $BINDIR)
 
-# allow building of specific targets
-if [ "$#" == 0 ]; then
-  TARGETS="${SRCDIR}/targets/*"
-elif [ "$#" == 1 ]; then
-  TARGETS="${SRCDIR}/targets/$1"
-else
-  TARGETS="${SRCDIR}/targets/$1"
-  shift
-  for arg in "$@"; do
-    TARGETS+=" ${SRCDIR}/targets/$arg"
-  done
+# we may already have the kernel rpm
+if [ ! -f "$BINARY"/${photon_kernel_rpm} ]; then
+     curl -L "$PHOTON_RPM_REPO"/${photon_kernel_rpm} -o "$BINARY"/${photon_kernel_rpm}
 fi
 
-export JOB=${JOB_NAME:-$NAME}_${BUILD_NUMBER:-local_build}
-DATE=$(date -u +%Y/%m/%d_@_%H:%M:%S)
+TCL_BASE=${TCL_BASE:-http://tinycorelinux.net/6.x}
+tcl_core_path=$TCL_BASE/x86_64/release/distribution_files/
+tcl_core=corepure64.gz
+tcl_tcz=$TCL_BASE/x86_64/tcz
+tcz_deps=(iproute2.tcz libtirpc.tcz)
 
-echo SRCDIR=${SRCDIR}
-echo BINDIR=${BINDIR}
-mkdir -p ${BINDIR}
+# Download the tcl core root fs
+if [ ! -f "$BINARY"/${tcl_core} ]; then
+     curl -L "${tcl_core_path}"/${tcl_core} -o "$BINARY"/${tcl_core}
+fi
 
-for i in $TARGETS; do
-  TNAME=${NAME}-$(basename $i)
-
-  git_args="--git-dir=$SRCDIR/../.git --work-tree=$SRCDIR/../"
-  branch_name="$(git $git_args symbolic-ref HEAD 2>/dev/null)" ||
-  branch_name="detached_head"     # detached HEAD
-  BRANCH=${branch_name##refs/heads/}
-  SHA=$(git $git_args rev-parse --short HEAD)
-
-  cp -r ${SRCDIR}/tether ${i}/tether
-
-  # if there is a base build, run that
-  if [ -d ${i}/base ]; then
-    docker build -t ${TNAME}-base ${i}/base || ( echo "Base build failed for $i" && break )
-    if [ $? -ne 0 ]; then
-      echo "Base build failed for $i: $?"
-      break
-    fi
-  fi
-  docker build --no-cache -t ${TNAME}-build ${i}
-  SUCCESS=$?
-
-
-  if [ $SUCCESS -eq 0 ]; then
-    BUILD_ID="$DATE@$BRANCH:$SHA"
-    docker run --name=$JOB-${TNAME} -e BUILD_ID=$BUILD_ID -e BINBASE=${BINBASE} -e ISOOUT=/binary/${TNAME}.iso ${TNAME}-build:latest && {
-      docker cp ${JOB}-${TNAME}:/tmp/${BINBASE} ${BINDIR}/..
-    }
-
-    SUCCESS=$?
-  fi
-
-  # clean up now the build's complete
-  rm -fr ${i}/tether
-  docker rm -v ${JOB}-${TNAME}
-
-  if [ $SUCCESS -ne 0 ]; then
-    echo "Build failed for $i: $SUCCESS"
-    break
-  fi
+# Download tinycore dependencies
+for dep in "${tcz_deps[@]}"; do
+     if [ ! -f "$BINARY"/$dep ]; then
+          echo "Downloading ${tcl_tcz}/$dep"
+          curl -L "${tcl_tcz}"/$dep -o "$BINARY"/$dep
+     fi
 done
 
-# make the return value for the script reflect the status
-test $SUCCESS -eq 0
+
+# Grab and build vendor if we havent already.
+if [ $( find "$vendor"/* -maxdepth 0 -type d | wc -l) -eq "0" ]; then
+     make -C ../ gvt vendor
+fi
+
+case $1 in
+     "docker")
+          (cd ../ && docker build \
+               -f=./Dockerfile.bootstrap-linux \
+               -t ${name}-"${sha}" \
+               --build-arg BUILD_ID="$BUILD_ID" \
+               --build-arg KERNEL_RPM="$BINARY"/${photon_kernel_rpm} \
+               --build-arg TINYCORE="$BINARY"/${tcl_core} \
+               --build-arg TINYCORE_DEPS="$BINARY/${tcz_deps[0]} $BINARY/${tcz_deps[1]}" \
+               .)
+          docker run --rm ${name}-"$sha":latest > "$isofile"
+     ;;
+
+     "local")
+          if [ "$(id -u)" != "0" ]; then
+               fakeroot=fakeroot
+          fi
+          make -C ../ bootstrap
+          BUILD_ID=$BUILD_ID $fakeroot ./build-iso.sh \
+               -i /usr/lib/syslinux/isolinux.bin \
+               -k "$BINARY"/$photon_kernel_rpm \
+               -c "$BINARY"/${tcl_core} \
+               -d "$BINARY/${tcz_deps[0]} $BINARY/${tcz_deps[1]}" \
+               "$isofile"
+     ;;
+
+     *)
+     usage
+     ;;
+esac
+
