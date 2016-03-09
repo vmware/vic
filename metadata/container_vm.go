463a41c245d0462f67c748c7af726c92853d026e
George Hicken <ghicken@vmware.com>
2016-03-09 08:19:09 -0800

    WIP - model of required metadata

--- metadata/container_vm.go
+++ metadata/container_vm.go
@@ -1,5 +1,99 @@
+// Copyright 2016 VMware, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package metadata
 
+import (
+	"net/url"
+	"os/exec"
+)
+
+// Common data between managed entities, across execution environments
+type Common struct {
+	// A reference to the components hosting execution environment, if any
+	ExecutionEnvironment string
+	// Unambiguous ID with meaning in the context of its hosting execution environment
+	ID string
+	// Convenience field to record a human readable name
+	Name string
+	// Freeform notes related to the entity
+	Notes string
+}
+
+// MountSpec details a mount that must be executed within the executor
+type MountSpec struct {
+	// The label that identifies which attached storage is the target
+	Label string
+	// The path in the executor at which this should be mounted
+	Path string
+	// Freeform mode string, which could translate directly to mount options
+	// We may want to turn this into a more structured form eventually
+	Mode string
+}
+
+// SessionControl defines the set of operations that can have a custom signal mapping
+// on a per session basis
+type SessionControl int
+
+// ContainerVM holds that data tightly associated with a containerVM, but that should not
+// be visible to the guest. This is the external complement to ExecutorConfig.
 type ContainerVM struct {
-    
+	Common
+
+	// The version of the bootstrap image that this container was booted from.
+	Version string
+
+	// Name aliases for this specific container, Maps alias to unambiguous name
+	// This uses unambiguous name rather than reified network endpoint to persist
+	// the intent rather than a point-in-time manifesting of that intent.
+	Aliases map[string]string
+	// The location of the interaction service that the tether should connect to. Examples:
+	// * tcp://x.x.x.x:2377
+	// * vmci://moid - should this be an moid or a VMCI CID? Does one insulate us from reboots?
+	Interaction url.URL
+}
+
+// ExecutorConfig holds the data tightly associated with an Executor. This is distinct from Sessions
+// in that there is no process inherently associated - this is closer to a ThreadPool than a Thread and
+// is the owner of the shared filesystem environment. This is the guest visible complement to ContainerVM.
+type ExecutorConfig struct {
+	Common
+
+	// Maps the mount name to the detail mount specification
+	Mounts map[string]MountSpec
+	// This describes an executors presence on a network, and contains sufficient
+	// information to configure the interface in the guest.
+	Networks map[string]NetworkEndpoint
+}
+
+// SessionConfig defines the content of a session - this maps to the root of a process tree
+// inside an executor
+// This is close to but not perfectly aligned with the new docker/docker/daemon/execdriver/driver:CommonProcessConfig
+type SessionConfig struct {
+	// The primary session may have the same ID as the executor owning it
+	Common
+
+	// The primary process for the session - this is an exec.Cmd only for now as it contains everything
+	// we need... along with some we don't
+	Cmd exec.Cmd
+	// Allocate a tty or not
+	Tty bool
+	// Maps the intent to the signal for this specific app
+	Signals map[SessionControl]int
+	// Use struct composition to add in the guest specific portions
+	// http://attilaolah.eu/2014/09/10/json-and-struct-composition-in-go/
+	// ulimits
+	// user
+	// rootfs - within the container context
 }
--- metadata/virtual_container_host.go
+++ metadata/virtual_container_host.go
@@ -1,3 +1,17 @@
+// Copyright 2016 VMware, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package metadata
 
 import (
@@ -10,41 +24,63 @@ import (
 	"golang.org/x/oauth2"
 )
 
+// Can we just treat the VCH appliance as a containerVM booting off a specific bootstrap image
+// It has many of the same requirements (around networks being attached, version recorded,
+// volumes mounted, et al). Each of the components can easily be captured as a Session given they
+// are simply processes.
+// This would require that the bootstrap read session record for the VM and relaunch them - that
+// actually aligns very well with containerVMs restarting their processes if restarted directly
+// (this is obviously a behaviour we'd want to toggles for in regular containers).
+
 // VirtualContainerHostConfigSpec holds the metadata for a Virtual Container Host that should be visible inside the appliance VM.
 type VirtualContainerHostConfigSpec struct {
+	// The base config for the appliance. This includes the networks that are to be attached
+	// and disks to be mounted.
+	// Networks are keyed by interface name
+	ExecutorConfig
+
+	// The set of components to launch
+	ComponentConfig []SessionConfig
+
 	// Virtual Container Host version
 	Version string
 
-	// The unambiguous identifer for the Virtual Container Host within a vSphere environment
-	MOID types.ManagedObjectReference
-	// Descriptive display name
-	Name string
 	// Administrative contact for the Virtual Container Host
-	VCHAdmin []mail.Address
-	// Administrative contact for vSphere
-	VSphereAdmin []mail.Address
+	Admin []mail.Address
+	// Administrative contact for hosting infrastructure
+	InfrastructureAdmin []mail.Address
 
-	// Certificates for user authentication
+	// Certificates for user authentication - this needs to be expanded to allow for directory server auth
 	UserCertificates []tls.Certificate
-	// Certificates for general network access, keyed by CIDR (IPNet.String())
+	// Certificates for general outgoing network access, keyed by CIDR (IPNet.String())
 	NetworkCertificates map[string]tls.Certificate
 	// Certificates for specific system access, keyed by FQDN
 	HostCertificates map[string]tls.Certificate
 
-	// Datastore URL for image store
-	ImageStore url.URL
-	// Datastore URL roots for volumes
+	// Port Layer - storage
+	// Datastore URLs for image stores - the top layer is [0], the bottom layer is [len-1]
+	ImageStores []url.URL
+	// Permitted datastore URL roots for volumes
 	VolumeLocations []url.URL
-	// Compute resource root
-	ComputeResource object.ComputeResource
+	// Permitted datastore URLs for container storage for this virtual container host
+	ContainerStores []url.URL
+	// Compute resource roots under which all containers will be created
+	ComputeResource []object.ComputeResource
 
-	// Networks to be attached to the Virtual Container Host, keyed by interface name
-	ApplianceNetworks map[string]NetworkEndpoint
-	// Published networks available for containers to join, keyed by consumption name
-	ContainerNetworks map[string]NetworkEndpoint
+	// Port Layer - network
 	// The default bridge network supplied for the Virtual Container Host
 	BridgeNetwork object.Network
+	// Published networks available for containers to join, keyed by consumption name
+	ContainerNetworks map[string]ContainerNetwork
 
+	// Port Layer - exec
+	// Default containerVM capacity
+	ContainerVMSize Resources
+
+	// Allow custom naming convention for containerVMs
+	ContainerNameConvention string
+
+	// Imagec
 	// Whitelist of registries
 	RegistryWhitelist []url.URL
 	// Blacklist of registries
@@ -59,18 +95,23 @@ type CustomerExperienceImprovementProgram struct {
 	Enabled bool
 }
 
-// Credentials is an exceptionally basic container for credentials
-type Credentials struct {
-	User        string
-	Password    []byte
-	Certificate []byte
-	Key         []byte
+// Resources is used instead of the ResourceAllocation structs in govmomi as
+// those don't currently hold IO or storage related data.
+type Resources struct {
+	CPU     types.ResourceAllocationInfo
+	Memory  types.ResourceAllocationInfo
+	IO      types.ResourceAllocationInfo
+	Storage types.ResourceAllocationInfo
 }
 
 // VirtualContainerHostTargetConfigSpec holds the metadata for a Virtual Container Host that should be visible only to the infrastructure on which it is running.
 type VirtualContainerHostTargetConfigSpec struct {
 	// Target and credentials for the environment - experimenting with storing this as an OAuth2 config
 	TargetConfig oauth2.Config
+	// Virtual Container Host capacity
+	VCHSize Resources
+	// Appliance capacity
+	ApplianceSize Resources
 	// Freeform notes for target administrator user
 	Notes string
 }
