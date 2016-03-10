54bf64c8e595cae589d3ca069d358d3092013935
George Hicken <ghicken@vmware.com>
2016-03-09 19:43:31 -0800

    Makefile updates and vicadmin insertion
    This makes significant changes to the Makefile, but to general layout, and
    to how the targets are resolved. This is intended to allow proper make
    behaviour with the generated code portions.
    This work was done to support vicadmin, so that's been included as well.
    This is vicadmin code from Bonneville, almost entirely unmodified - this
    will need significant update in the coming months.

---
+++ vicadmin/vicadm_test.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func init() {
+	sdk := os.Getenv("GOVC_URL")
+	if sdk != "" {
+		flag.Set("sdk", sdk)
+		flag.Set("vm-path", "docker-appliance")
+		flag.Set("cluster", os.Getenv("GOVC_CLUSTER"))
+	}
+
+	docker := os.Getenv("DOCKER_HOST")
+	if sdk != "" {
+		u, err := url.Parse(docker)
+		if err != nil {
+			log.Fatal(err)
+		}
+		flag.Set("docker-host", u.Host)
+	}
+}
+
+func TestLogTar(t *testing.T) {
+	logFiles = []string{"vicadm.go", "vicadm_test.go"}
+
+	s := &server{
+		addr: "127.0.0.1:0",
+	}
+
+	err := s.listen()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port := s.listenPort()
+
+	go s.serve()
+
+	res, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d/container-logs.tar.gz", port))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	z, err := gzip.NewReader(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tz := tar.NewReader(z)
+
+	for {
+		h, err := tz.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		name, err := url.QueryUnescape(h.Name)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if testing.Verbose() {
+			fmt.Printf("\n%s...\n", name)
+			io.CopyN(os.Stdout, tz, 150)
+			fmt.Printf("...\n")
+		}
+	}
+}
+
+func TestLogTail(t *testing.T) {
+	f, err := ioutil.TempFile("", "vicadm")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+
+	f.WriteString("# not much here yet\n")
+
+	logFiles = []string{f.Name(), "vicadm.go"}
+	name := filepath.Base(f.Name())
+
+	s := &server{
+		addr: "127.0.0.1:0",
+	}
+
+	err = s.listen()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port := s.listenPort()
+
+	go s.serve()
+
+	out := ioutil.Discard
+	if testing.Verbose() {
+		out = os.Stdout
+	}
+
+	paths := []string{
+		"/logs/tail/" + name,
+		"/logs/tail",
+		"/logs/" + name,
+		"/",
+	}
+
+	i := 0
+
+	u := url.URL{
+		Scheme: "http",
+		Host:   fmt.Sprintf("127.0.0.1:%d", port),
+	}
+
+	for _, path := range paths {
+		u.Path = path
+		log.Printf("GET %s:\n", u)
+		res, err := http.Get(u.String())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		go func() {
+			for j := 0; j < 512; j++ {
+				i++
+				f.WriteString(fmt.Sprintf("this is line %d\n", i))
+			}
+		}()
+
+		size := int64(256)
+		n, _ := io.CopyN(out, res.Body, size)
+		out.Write([]byte("...\n"))
+		res.Body.Close()
+
+		if n != size {
+			t.Errorf("expected %d, got %d", size, n)
+		}
+	}
+}
