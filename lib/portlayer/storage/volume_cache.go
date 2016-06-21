40b660882b8b69af9505f4d369eec875c57b5873
Faiyaz Ahmed <ahmedf@vmware.com>
2016-06-20 17:45:48 -0700

    Add Volume API to portlayer
    Includes a VolumeStorer interface defenition to store/retrieve/remove
    volumes.
    
    Implements a cache implementation of the interface so as to avoid
    unnecessary datastore operations.

---
+++ lib/portlayer/storage/volume_cache.go
@@ -0,0 +1,137 @@
+// Copyright 2016 VMware, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"net/url"
+	"os"
+	"sync"
+
+	log "github.com/Sirupsen/logrus"
+	"golang.org/x/net/context"
+)
+
+// VolumeLookupCache caches Volume references to volumes in the system.
+type VolumeLookupCache struct {
+
+	// Maps IDs to Volumes.
+	//
+	// id -> Volume
+	vlc     map[string]Volume
+	vlcLock sync.Mutex
+
+	// The underlying data storage implementation
+	volumeStore VolumeStorer
+}
+
+func NewVolumeLookupCache(ctx context.Context, vs VolumeStorer) (*VolumeLookupCache, error) {
+	v := &VolumeLookupCache{
+		vlc:         make(map[string]Volume),
+		volumeStore: vs,
+	}
+
+	return v, v.rebuildCache(ctx)
+}
+
+func (v *VolumeLookupCache) VolumeCreate(ctx context.Context, ID string, store *url.URL, capacityMB uint64, info map[string][]byte) (*Volume, error) {
+	v.vlcLock.Lock()
+	defer v.vlcLock.Unlock()
+
+	// check if it exists
+	_, ok := v.vlc[ID]
+	if ok {
+		return nil, os.ErrExist
+	}
+
+	vol, err := v.volumeStore.VolumeCreate(ctx, ID, store, capacityMB, info)
+	if err != nil {
+		return nil, err
+	}
+	// Add it to the cache.
+	v.vlc[vol.ID] = *vol
+
+	return vol, nil
+}
+
+func (v *VolumeLookupCache) VolumeDestroy(ctx context.Context, ID string) error {
+	v.vlcLock.Lock()
+	defer v.vlcLock.Unlock()
+
+	// Check if it exists
+	vol, ok := v.vlc[ID]
+	if !ok {
+		return os.ErrNotExist
+	}
+
+	// remove it from the volumestore
+	if err := v.volumeStore.VolumeDestroy(ctx, ID); err != nil {
+		return err
+	}
+	delete(v.vlc, vol.ID)
+
+	return nil
+}
+
+func (v *VolumeLookupCache) VolumeGet(ctx context.Context, ID string) (*Volume, error) {
+	v.vlcLock.Lock()
+	defer v.vlcLock.Unlock()
+
+	// look in the cache
+	vol, ok := v.vlc[ID]
+	if !ok {
+		return nil, os.ErrNotExist
+	}
+
+	return &vol, nil
+}
+
+func (v *VolumeLookupCache) VolumesList(ctx context.Context) ([]*Volume, error) {
+	v.vlcLock.Lock()
+	defer v.vlcLock.Unlock()
+
+	// look in the cache, return the list
+	l := make([]*Volume, 0, len(v.vlc))
+	for _, vol := range v.vlc {
+		// this is idiotic
+		var e Volume
+		e = vol
+		l = append(l, &e)
+	}
+
+	return l, nil
+}
+
+// goto the volume store and repopulate the cache.
+func (v *VolumeLookupCache) rebuildCache(ctx context.Context) error {
+
+	// Lock everything because we're rewriting the whole cache
+	v.vlcLock.Lock()
+	defer v.vlcLock.Unlock()
+
+	log.Info("Refreshing volume cache.")
+	// if it's not in the cache, check the volumeStore, cache the result, and return the list.
+	vols, err := v.volumeStore.VolumesList(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, vol := range vols {
+		log.Infof("Volumestore: Found vol %s on store %s.", vol.ID, vol.Store)
+		// Add it to the cache.
+		v.vlc[vol.ID] = *vol
+	}
+
+	return nil
+}
---
+++ lib/portlayer/storage/volume_cache_test.go
@@ -0,0 +1,219 @@
+// Copyright 2016 VMware, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+	"sync"
+	"testing"
+
+	"golang.org/x/net/context"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vmware/vic/lib/portlayer/util"
+)
+
+type MockVolumeStore struct {
+	// id -> volume
+	db map[string]*Volume
+}
+
+func NewMockVolumeStore() *MockVolumeStore {
+	m := &MockVolumeStore{
+		db: make(map[string]*Volume),
+	}
+
+	return m
+}
+
+// Creates a volume on the given volume store, of the given size, with the given metadata.
+func (m *MockVolumeStore) VolumeCreate(ctx context.Context, ID string, store *url.URL, capacityMB uint64, info map[string][]byte) (*Volume, error) {
+	storeName, err := util.VolumeStoreName(store)
+	if err != nil {
+		return nil, err
+	}
+
+	selfLink, err := util.VolumeURL(storeName, ID)
+	if err != nil {
+		return nil, err
+	}
+
+	vol := &Volume{
+		ID:       ID,
+		Store:    store,
+		SelfLink: selfLink,
+	}
+
+	m.db[ID] = vol
+
+	return vol, nil
+}
+
+// Get an existing volume via it's ID and volume store.
+func (m *MockVolumeStore) VolumeGet(ctx context.Context, ID string) (*Volume, error) {
+	vol, ok := m.db[ID]
+	if !ok {
+		return nil, os.ErrNotExist
+	}
+
+	return vol, nil
+}
+
+// Destroys a volume
+func (m *MockVolumeStore) VolumeDestroy(ctx context.Context, ID string) error {
+	if _, ok := m.db[ID]; !ok {
+		return os.ErrNotExist
+	}
+
+	delete(m.db, ID)
+
+	return nil
+}
+
+// Lists all volumes on the given volume store`
+func (m *MockVolumeStore) VolumesList(ctx context.Context) ([]*Volume, error) {
+	var i int
+	list := make([]*Volume, len(m.db))
+	for _, v := range m.db {
+		t := *v
+		list[i] = &t
+		i++
+	}
+
+	return list, nil
+}
+
+func TestVolumeCreateGetListAndDelete(t *testing.T) {
+	mvs := NewMockVolumeStore()
+	v, err := NewVolumeLookupCache(context.TODO(), mvs)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	storeURL, err := util.VolumeStoreNameToURL("testStore")
+	if !assert.NoError(t, err) || !assert.NotNil(t, storeURL) {
+		return
+	}
+
+	inVols := make(map[string]*Volume)
+	wg := &sync.WaitGroup{}
+	createFn := func(i int) {
+		defer wg.Done()
+
+		id := fmt.Sprintf("ID-%d", i)
+
+		// Write to the datastore
+		vol, err := v.VolumeCreate(context.TODO(), id, storeURL, 0, nil)
+		if !assert.NoError(t, err) || !assert.NotNil(t, vol) {
+			return
+		}
+
+		inVols[id] = vol
+	}
+
+	// Create a set of volumes
+	numVolumes := 5
+	wg.Add(numVolumes)
+	for i := 0; i < numVolumes; i++ {
+		go createFn(i)
+	}
+	wg.Wait()
+
+	getFn := func(inVol *Volume) {
+		vol, err := v.VolumeGet(context.TODO(), inVol.ID)
+		if !assert.NoError(t, err) || !assert.NotNil(t, vol) {
+			return
+		}
+
+		if !assert.Equal(t, inVol, vol) {
+			return
+		}
+		wg.Done()
+	}
+
+	wg.Add(numVolumes)
+	for _, inVol := range inVols {
+		getFn(inVol)
+	}
+	wg.Wait()
+
+	volumeList, err := v.VolumesList(context.TODO())
+	if !assert.NoError(t, err) || !assert.Equal(t, numVolumes, len(volumeList)) {
+		return
+	}
+
+	// Test that the list returned by VolumeList matches our inVols list.  Then
+	// delete each vol via the cache, then check the datastore to ensure it's
+	// empty
+	for _, outVol := range volumeList {
+		if !assert.Equal(t, inVols[outVol.ID], outVol) {
+			return
+		}
+
+		if err = v.VolumeDestroy(context.TODO(), outVol.ID); !assert.NoError(t, err) {
+			return
+		}
+	}
+
+	// check the datastore is empty.
+	if !assert.Empty(t, mvs.db) {
+		return
+	}
+}
+
+// Create 2 store caches but use the same backing datastore.  Create images
+// with the first cache, then get the image with the second.  This simulates
+// restart since the second cache is empty and has to go to the backing store.
+func TestVolumeCacheRestart(t *testing.T) {
+	mvs := NewMockVolumeStore()
+	firstCache, err := NewVolumeLookupCache(context.TODO(), mvs)
+	if !assert.NoError(t, err) || !assert.NotNil(t, firstCache) {
+		return
+	}
+
+	storeURL, err := util.VolumeStoreNameToURL("testStore")
+	if !assert.NoError(t, err) || !assert.NotNil(t, storeURL) {
+		return
+	}
+
+	// Create a set of volumes
+	inVols := make(map[string]*Volume)
+	for i := 1; i < 50; i++ {
+		id := fmt.Sprintf("ID-%d", i)
+
+		// Write to the datastore
+		vol, err := firstCache.VolumeCreate(context.TODO(), id, storeURL, 0, nil)
+		if !assert.NoError(t, err) || !assert.NotNil(t, vol) {
+			return
+		}
+
+		inVols[id] = vol
+	}
+
+	secondCache, err := NewVolumeLookupCache(context.TODO(), mvs)
+	if !assert.NoError(t, err) || !assert.NotNil(t, secondCache) {
+		return
+	}
+
+	// get the vols from the second cache to ensure it goes to the ds
+	for _, expectedVol := range inVols {
+		vol, err := secondCache.VolumeGet(context.TODO(), expectedVol.ID)
+		if !assert.NoError(t, err) || !assert.Equal(t, expectedVol, vol) {
+			return
+		}
+	}
+}
