c3f6129b504b1752301608c91fb2418f81e15384
Ian Whitlock <iwhitlock@vmware.com>
2016-06-24 15:46:00 -0500

    Don't exit installer on failure to unregister extension

--- cmd/vic-machine/delete/delete.go
+++ cmd/vic-machine/delete/delete.go
@@ -146,10 +146,10 @@ func (d *Uninstall) Run(cli *cli.Context) error {
 	if validator.IsVC() {
 		log.Infoln("Removing VCH vSphere extension")
 		if err = executor.GenerateExtensionName(vchConfig); err != nil {
-			return err
+			log.Errorf("Wasn't able to get extension name during VCH deletion. Failed with error: %s", err)
 		}
 		if err = executor.UnregisterExtension(vchConfig.ExtensionName); err != nil {
-			return err
+			log.Errorf("Wasn't able to remove extension %s due to error: %s", vchConfig.ExtensionName, err)
 		}
 	}
 
