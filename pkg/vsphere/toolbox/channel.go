ef831f63834c0503b03445702a05627bdaf5728a
Doug MacEachern <dougm@vmware.com>
2016-07-07 15:47:54 -0700

    Guest tools framework improvements
    - Implement Capabilities_Register RPC
    
    - Use the same polling interval and backoff logic as vmtoolsd
    
    - Add support for guest power operations
    
    - Trace disabled by default
    
    - Add ChannelOut.Request wrapper
    
    - Add ESX tests, additional unit tests
    
    Closes issue #742

--- pkg/vsphere/toolbox/channel.go
+++ pkg/vsphere/toolbox/channel.go
@@ -14,10 +14,43 @@
 
 package toolbox
 
+import (
+	"bytes"
+	"fmt"
+)
+
 // Channel abstracts the guest<->vmx RPC transport
 type Channel interface {
 	Start() error
 	Stop() error
 	Send([]byte) error
 	Receive() ([]byte, error)
+}
+
+var (
+	rpciOK  = []byte{'1', ' '}
+	rpciERR = []byte{'0', ' '}
+)
+
+// ChannelOut extends Channel to provide RPCI protocol helpers
+type ChannelOut struct {
+	Channel
+}
+
+// Request sends an RPC command to the vmx and checks the return code for success or error
+func (c *ChannelOut) Request(request []byte) ([]byte, error) {
+	if err := c.Send(request); err != nil {
+		return nil, err
+	}
+
+	reply, err := c.Receive()
+	if err != nil {
+		return nil, err
+	}
+
+	if bytes.HasPrefix(reply, rpciOK) {
+		return reply[2:], nil
+	}
+
+	return nil, fmt.Errorf("request %q: %q", request, reply)
 }
--- pkg/vsphere/toolbox/service_test.go
+++ pkg/vsphere/toolbox/service_test.go
@@ -15,9 +15,15 @@
 package toolbox
 
 import (
+	"bytes"
+	"errors"
+	"flag"
 	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"sync"
 	"testing"
-	"time"
 )
 
 func TestDefaultIP(t *testing.T) {
@@ -25,7 +31,6 @@ func TestDefaultIP(t *testing.T) {
 	if ip == "" {
 		t.Error("failed to get a default IP address")
 	}
-	t.Logf("DefaultIP=%s", ip)
 }
 
 type testRPC struct {
@@ -37,10 +42,12 @@ type mockChannelIn struct {
 	t       *testing.T
 	service *Service
 	rpc     []*testRPC
+	wg      sync.WaitGroup
+	start   error
 }
 
 func (c *mockChannelIn) Start() error {
-	return nil
+	return c.start
 }
 
 func (c *mockChannelIn) Stop() error {
@@ -49,8 +56,11 @@ func (c *mockChannelIn) Stop() error {
 
 func (c *mockChannelIn) Receive() ([]byte, error) {
 	if len(c.rpc) == 0 {
-		// Stop the service after all test RPC requests have been consumed
-		defer c.service.Stop()
+		if c.rpc != nil {
+			// All test RPC requests have been consumed
+			c.wg.Done()
+			c.rpc = nil
+		}
 		return nil, io.EOF
 	}
 
@@ -64,7 +74,7 @@ func (c *mockChannelIn) Send(buf []byte) error {
 
 	expect := c.rpc[0].expect
 	if string(buf) != expect {
-		c.t.Errorf("expected '%s' reply for request '%s', got: '%s'", expect, c.rpc[0].cmd, string(buf))
+		c.t.Errorf("expected %q reply for request %q, got: %q", expect, c.rpc[0].cmd, buf)
 	}
 
 	c.rpc = c.rpc[1:]
@@ -73,10 +83,13 @@ func (c *mockChannelIn) Send(buf []byte) error {
 }
 
 // discard rpc out for now
-type mockChannelOut struct{}
+type mockChannelOut struct {
+	reply [][]byte
+	start error
+}
 
 func (c *mockChannelOut) Start() error {
-	return nil
+	return c.start
 }
 
 func (c *mockChannelOut) Stop() error {
@@ -84,29 +97,45 @@ func (c *mockChannelOut) Stop() error {
 }
 
 func (c *mockChannelOut) Receive() ([]byte, error) {
-	panic("receive on out channel")
+	if len(c.reply) == 0 {
+		return nil, io.EOF
+	}
+	reply := c.reply[0]
+	c.reply = c.reply[1:]
+	return reply, nil
 }
 
 func (c *mockChannelOut) Send(buf []byte) error {
+	if len(buf) == 0 {
+		return io.ErrShortBuffer
+	}
 	return nil
 }
 
 func TestServiceRun(t *testing.T) {
-	Trace = testing.Verbose()
-
 	in := new(mockChannelIn)
 	out := new(mockChannelOut)
 
 	service := NewService(in, out)
-	service.Interval = time.Millisecond
 
 	in.rpc = []*testRPC{
 		{"reset", "OK ATR toolbox"},
 		{"ping", "OK "},
 		{"Set_Option synctime 0", "OK "},
-		{"NOPE", "Unknown Command"},
+		{"Capabilities_Register", "OK "},
 		{"Set_Option broadcastIP 1", "OK "},
 	}
+
+	in.wg.Add(1)
+
+	// replies to register capabilities
+	for i := 0; i < len(capabilities); i++ {
+		out.reply = append(out.reply, rpciOK)
+	}
+
+	out.reply = append(out.reply,
+		rpciOK, // reply to IP broadcast
+	)
 
 	in.service = service
 
@@ -117,5 +146,177 @@ func TestServiceRun(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	in.wg.Wait()
+
+	service.Stop()
+	service.Wait()
+
+	// verify we don't set delay > maxDelay
+	for i := 0; i <= maxDelay+1; i++ {
+		service.backoff()
+	}
+
+	if service.delay != maxDelay {
+		t.Errorf("delay=%d", service.delay)
+	}
+}
+
+func TestServiceErrors(t *testing.T) {
+	Trace = true
+	if !testing.Verbose() {
+		// cover TraceChannel but discard output
+		traceLog = ioutil.Discard
+	}
+
+	netInterfaceAddrs = func() ([]net.Addr, error) {
+		return nil, io.EOF
+	}
+
+	in := new(mockChannelIn)
+	out := new(mockChannelOut)
+
+	service := NewService(in, out)
+
+	service.RegisterHandler("Sorry", func([]byte) ([]byte, error) {
+		return nil, errors.New("i am so sorry")
+	})
+
+	in.rpc = []*testRPC{
+		{"Capabilities_Register", "OK "},
+		{"Set_Option broadcastIP 1", "ERR "},
+		{"NOPE", "Unknown Command"},
+		{"Sorry", "ERR "},
+	}
+
+	in.wg.Add(1)
+
+	// replies to register capabilities
+	for i := 0; i < len(capabilities); i++ {
+		out.reply = append(out.reply, rpciERR)
+	}
+
+	foo := []byte("foo")
+	out.reply = append(
+		out.reply,
+		rpciERR,
+		append(rpciOK, foo...),
+		rpciERR,
+	)
+
+	in.service = service
+
+	in.t = t
+
+	err := service.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	in.wg.Wait()
+
+	// Done serving RPCs, test ChannelOut errors
+	reply, err := service.out.Request(rpciOK)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !bytes.Equal(reply, foo) {
+		t.Errorf("reply=%q", foo)
+	}
+
+	_, err = service.out.Request(rpciOK)
+	if err == nil {
+		t.Error("expected error")
+	}
+
+	_, err = service.out.Request(nil)
+	if err == nil {
+		t.Error("expected error")
+	}
+
+	service.Stop()
+	service.Wait()
+
+	// cover service start error paths
+	start := errors.New("fail")
+
+	in.start = start
+	err = service.Start()
+	if err != start {
+		t.Error("expected error")
+	}
+
+	in.start = nil
+	out.start = start
+	err = service.Start()
+	if err != start {
+		t.Error("expected error")
+	}
+}
+
+var (
+	testESX = flag.Bool("toolbox.testesx", false, "Test toolbox service against ESX (vmtoolsd must not be running)")
+	testPID = flag.Int("toolbox.testpid", 0, "PID to return from toolbox start command")
+)
+
+func TestServiceRunESX(t *testing.T) {
+	if *testESX == false {
+		t.SkipNow()
+	}
+
+	Trace = testing.Verbose()
+
+	var wg sync.WaitGroup
+
+	in := NewBackdoorChannelIn()
+	out := NewBackdoorChannelOut()
+
+	service := NewService(in, out)
+
+	// assert that reset, ping, Set_Option and Capabilities_Register are called at least once
+	for name, handler := range service.handlers {
+		n := name
+		h := handler
+		wg.Add(1)
+
+		service.handlers[name] = func(b []byte) ([]byte, error) {
+			defer wg.Done()
+
+			service.handlers[n] = h // reset
+
+			return h(b)
+		}
+	}
+
+	vix := RegisterVixRelayedCommandHandler(service)
+
+	if *testPID != 0 {
+		wg.Add(1)
+		vix.ProcessStartCommand = func(r *VixMsgStartProgramRequest) (int, error) {
+			defer wg.Done()
+
+			if r.ProgramPath != "/bin/date" {
+				t.Errorf("ProgramPath=%q", r.ProgramPath)
+			}
+
+			return *testPID, nil
+		}
+	}
+
+	wg.Add(1)
+	service.PrimaryIP = func() string {
+		defer wg.Done()
+		log.Print("broadcasting IP")
+		return DefaultIP()
+	}
+
+	err := service.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wg.Wait()
+
+	service.Stop()
 	service.Wait()
 }
