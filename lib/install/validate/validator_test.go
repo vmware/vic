eecf64db0f935932dfacedad464ac628c9cdbffe
Emma <line@vmware.com>
2016-07-08 14:46:23 -0500

    Add ut for install/validate
      Fix integration test doc

--- lib/install/validate/validator_test.go
+++ lib/install/validate/validator_test.go
@@ -15,9 +15,20 @@
 package validate
 
 import (
+	"net/url"
 	"testing"
 
+	log "github.com/Sirupsen/logrus"
+
 	"github.com/stretchr/testify/assert"
+	"github.com/vmware/govmomi/vim25/types"
+	"github.com/vmware/vic/lib/install/data"
+	"github.com/vmware/vic/lib/metadata"
+	"github.com/vmware/vic/pkg/trace"
+	"github.com/vmware/vic/pkg/vsphere/session"
+	"github.com/vmware/vic/pkg/vsphere/simulator"
+
+	"golang.org/x/net/context"
 )
 
 type TestValidator struct {
@@ -54,5 +65,214 @@ func TestSampleConversion(t *testing.T) {
 	for in, expected := range translations {
 		ipath := v.computePathToInventoryPath(in)
 		assert.Equal(t, expected, ipath, "Translation did not match")
+	}
+}
+
+func TestMain(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	trace.Logger.Level = log.DebugLevel
+	ctx := context.Background()
+
+	for i, model := range []*simulator.Model{simulator.ESX(), simulator.VPX()} {
+		t.Logf("%d", i)
+		defer model.Remove()
+		err := model.Create()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		s := model.Service.NewServer()
+		defer s.Close()
+
+		s.URL.User = url.UserPassword("user", "pass")
+		s.URL.Path = ""
+		t.Logf("server URL: %s", s.URL)
+
+		var input *data.Data
+		if i == 0 {
+			input = getESXData(s.URL)
+		} else {
+			input = getVPXData(s.URL)
+		}
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		validator, err := NewValidator(ctx, input)
+		if err != nil {
+			t.Errorf("Failed to new validator, %s", err)
+		}
+
+		validator.DisableFirewallCheck = true
+		validator.DisableDRSCheck = true
+		t.Logf("session pool: %s", validator.Session.Pool)
+		if err = createPool(ctx, validator.Session, input.ComputeResourcePath, "validator", t); err != nil {
+			t.Errorf("Unable to create resource pool: %s", err)
+		}
+
+		conf := testCompute(validator, input, t)
+		testTargets(validator, input, conf, t)
+		testStorage(validator, input, conf, t)
+		//		testNetwork() need dvs support
+	}
+}
+
+func getESXData(url *url.URL) *data.Data {
+	result := data.NewData()
+	url.Path = url.Path + "/ha-datacenter"
+	result.URL = url
+	result.DisplayName = "test001"
+	result.ComputeResourcePath = "/ha-datacenter/host/localhost.localdomain/Resources"
+	result.ImageDatastoreName = "LocalDS_0"
+	result.BridgeNetworkName = "bridge"
+	result.ManagementNetworkName = "VM Network"
+	result.ExternalNetworkName = "VM Network"
+	result.VolumeLocations = make(map[string]string)
+	result.VolumeLocations["volume-store"] = "LocalDS_0/volumes/test"
+
+	return result
+}
+
+func getVPXData(url *url.URL) *data.Data {
+	result := data.NewData()
+	url.Path = url.Path + "/DC0"
+	result.URL = url
+	result.DisplayName = "test001"
+	result.ComputeResourcePath = "/DC0/host/DC0_C0/Resources"
+	result.ImageDatastoreName = "LocalDS_0"
+	result.ExternalNetworkName = "VM Network"
+	result.BridgeNetworkName = "bridge"
+	result.VolumeLocations = make(map[string]string)
+	result.VolumeLocations["volume-store"] = "LocalDS_0/volumes/test"
+
+	return result
+}
+
+func createPool(ctx context.Context, sess *session.Session, poolPath string, name string, t *testing.T) error {
+	rp, err := sess.Finder.ResourcePool(ctx, poolPath)
+	if err != nil {
+		t.Logf("Failed to get parent pool, %s", err)
+		return err
+	}
+	t.Logf("Creating Resource Pool %s", name)
+	resSpec := types.ResourceConfigSpec{
+		CpuAllocation: &types.ResourceAllocationInfo{
+			Shares: &types.SharesInfo{
+				Level: types.SharesLevelNormal,
+			},
+			ExpandableReservation: types.NewBool(true),
+			Limit:       -1,
+			Reservation: 1,
+		},
+		MemoryAllocation: &types.ResourceAllocationInfo{
+			Shares: &types.SharesInfo{
+				Level: types.SharesLevelNormal,
+			},
+			ExpandableReservation: types.NewBool(true),
+			Limit:       -1,
+			Reservation: 1,
+		},
+	}
+
+	_, err = rp.Create(ctx, name, resSpec)
+	if err != nil {
+		t.Logf("Failed to create resource pool %s: %s", name, err)
+		return err
+	}
+	return nil
+}
+
+func testCompute(v *Validator, input *data.Data, t *testing.T) *metadata.VirtualContainerHostConfigSpec {
+	tests := []struct {
+		path   string
+		vc     bool
+		hasErr bool
+	}{
+		{"DC0_C0/Resources/validator", true, false},
+		{"DC0_C0/validator", true, false},
+		{"validator", true, true},
+		{"DC0_C0/test", true, true},
+		{"/DC0_C1/test", true, true},
+		{"/DC0_C1/test", true, true},
+		{"/DC0/host/DC0_C1/Resources/validator", true, true},
+		{"/DC1/host/DC0_C1/Resources/validator", true, true},
+		{"DC0_H0/Resources", true, false},
+		{"DC0_H0", true, false},
+		{"/DC0/host/DC0_C0/Resources/validator", true, false},
+		{"localhost.localdomain/Resources/validator", false, false},
+		{"validator", false, false},
+		{"test", false, true},
+		{"/ha-datacenter/host/localhost.localdomain/Resources/validator", false, false},
+	}
+	conf := &metadata.VirtualContainerHostConfigSpec{}
+
+	for _, test := range tests {
+		if v.isVC && !test.vc {
+			continue
+		}
+		if !v.isVC && test.vc {
+			continue
+		}
+		t.Logf("%+v", test)
+		input.ComputeResourcePath = test.path
+		v.compute(v.Context, input, conf)
+		v.ListIssues()
+		if !test.hasErr {
+			assert.Equal(t, 0, len(v.issues))
+		} else {
+			assert.True(t, len(v.issues) > 0, "Should have errors")
+		}
+		v.issues = nil
+	}
+	return conf
+}
+
+func testTargets(v *Validator, input *data.Data, conf *metadata.VirtualContainerHostConfigSpec, t *testing.T) {
+	v.target(v.Context, input, conf)
+	pass, set := conf.Target.User.Password()
+	t.Logf("target: %+v", conf.Target)
+	if v.isVC {
+		assert.Equal(t, pass, "")
+		assert.False(t, set, "Should not contain password")
+	} else {
+		assert.Equal(t, pass, "pass")
+		assert.True(t, set, "Should contain password in ESXi")
+	}
+}
+
+func testStorage(v *Validator, input *data.Data, conf *metadata.VirtualContainerHostConfigSpec, t *testing.T) {
+	tests := []struct {
+		image         string
+		volumes       map[string]string
+		hasErr        bool
+		expectImage   string
+		expectVolumes map[string]string
+	}{
+		{"LocalDS_0", map[string]string{"volume1": "LocalDS_0/volumes/volume1", "volume2": "ds://LocalDS_0/volumes/volume2"}, false, "ds://LocalDS_0", map[string]string{"volume1": "ds://LocalDS_0/volumes/volume1", "volume2": "ds://LocalDS_0/volumes/volume2"}},
+		{"ds://LocalDS_0/images", map[string]string{"volume1": "LocalDS_0/volumes/volume1", "volume2": "ds://LocalDS_0/volumes/volume2"}, false, "ds://LocalDS_0", map[string]string{"volume1": "ds://LocalDS_0/volumes/volume1", "volume2": "ds://LocalDS_0/volumes/volume2"}},
+		{"LocalDS_0/images", map[string]string{"volume1": "LocalDS_0/volumes/volume1", "volume2": "ds://LocalDS_0/volumes/volume2"}, false, "ds://LocalDS_0", map[string]string{"volume1": "ds://LocalDS_0/volumes/volume1", "volume2": "ds://LocalDS_0/volumes/volume2"}},
+		{"ds://LocalDS_0/images/xyz", map[string]string{"volume1": "LocalDS_0/volumes/volume1", "volume2": "ds://LocalDS_0/volumes/volume2"}, false, "ds://LocalDS_0", map[string]string{"volume1": "ds://LocalDS_0/volumes/volume1", "volume2": "ds://LocalDS_0/volumes/volume2"}},
+		{"LocalDS_0", map[string]string{"volume1": "LocalDS_1/volumes/volume1", "volume2": "ds://LocalDS_1/volumes/volume2"}, true, "", nil},
+		{"ds://LocalDS_0", map[string]string{"volume1": "LocalDS_1/volumes/volume1", "volume2": "ds://LocalDS_1/volumes/volume2"}, true, "", nil},
+		{"", map[string]string{"volume1": "", "volume2": "ds://"}, true, "", nil},
+		{"ds://", map[string]string{"volume1": "", "volume2": "ds://"}, true, "", nil},
+	}
+
+	for _, test := range tests {
+		t.Logf("%+v", test)
+		input.ImageDatastoreName = test.image
+		input.VolumeLocations = test.volumes
+		v.storage(v.Context, input, conf)
+		v.ListIssues()
+		if !test.hasErr {
+			assert.Equal(t, 0, len(v.issues))
+			assert.Equal(t, test.expectImage, conf.ImageStores[0].String())
+			for key, volume := range conf.VolumeLocations {
+				assert.Equal(t, test.expectVolumes[key], volume.String())
+			}
+		} else {
+			assert.True(t, len(v.issues) > 0, "Should have errors")
+		}
+		v.issues = nil
 	}
 }
