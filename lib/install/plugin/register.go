bd37e14249b7706dd5df04f999a5bed8122e8ac1
Andrew Chin <andrewchin@vmware.com>
2016-07-13 18:57:54 -0700

    Add vic-machine ui install and remove

---
+++ lib/install/plugin/register.go
@@ -0,0 +1,212 @@
+// Copyright 2016 VMware, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plugin
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/vim25/types"
+	"github.com/vmware/vic/pkg/trace"
+	"github.com/vmware/vic/pkg/vsphere/session"
+
+	"golang.org/x/net/context"
+)
+
+type Info struct {
+	Company               string
+	Key                   string
+	Name                  string
+	ShowInSolutionManager bool
+	Summary               string
+	Type                  string
+	URL                   string
+	Version               string
+}
+
+type Pluginer struct {
+	Session          *session.Session
+	ExtensionManager *object.ExtensionManager
+	Context          context.Context
+
+	info *Info
+
+	tURL      *url.URL
+	connected bool
+}
+
+func NewPluginer(ctx context.Context, target *url.URL, i *Info) (*Pluginer, error) {
+	defer trace.End(trace.Begin(""))
+
+	p := &Pluginer{
+		tURL: target,
+		info: i,
+	}
+	p.Context = ctx
+
+	err := p.connect()
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+func (p *Pluginer) disconnect() error {
+	defer trace.End(trace.Begin(""))
+	if p.Session != nil {
+		err := p.Session.Client.Logout(p.Context)
+		if err != nil {
+			return fmt.Errorf("failed to disconnect: %s", err)
+		}
+	}
+	p.connected = false
+	return nil
+}
+
+func (p *Pluginer) connect() error {
+	defer trace.End(trace.Begin(""))
+	var err error
+
+	sessionconfig := &session.Config{
+		Insecure: true,
+	}
+	sessionconfig.Service = p.tURL.String()
+
+	p.Session = session.NewSession(sessionconfig)
+	p.Session, err = p.Session.Connect(p.Context)
+	if err != nil {
+		return fmt.Errorf("failed to connect: %s", err)
+	}
+
+	p.Session.Populate(p.Context)
+
+	em, err := object.GetExtensionManager(p.Session.Client.Client)
+	if err != nil {
+		return fmt.Errorf("failed to get extension manager: %s", err)
+	}
+	p.ExtensionManager = em
+
+	p.connected = true
+	return nil
+}
+
+// Register installs an extension to the target
+func (p *Pluginer) Register() error {
+	defer trace.End(trace.Begin(""))
+	var err error
+	if !p.connected {
+		return errors.New("not connected")
+	}
+
+	desc := types.Description{
+		Label:   p.info.Name,
+		Summary: p.info.Summary,
+	}
+
+	e := types.Extension{
+		Key:         p.info.Key,
+		Version:     p.info.Version,
+		Company:     p.info.Company,
+		Description: &desc,
+	}
+
+	eci := types.ExtensionClientInfo{
+		Version:     p.info.Version,
+		Company:     p.info.Company,
+		Description: &desc,
+		Type:        p.info.Type,
+		Url:         p.info.URL,
+	}
+	e.Client = append(e.Client, eci)
+
+	d := types.KeyValue{
+		Key:   "name",
+		Value: p.info.Name,
+	}
+
+	eri := types.ExtensionResourceInfo{
+		Locale: "en_US",
+		Module: "name",
+	}
+
+	eri.Data = append(eri.Data, d)
+
+	e.ResourceList = append(e.ResourceList, eri)
+
+	// TODO HTTP server info
+
+	e.ShownInSolutionManager = &p.info.ShowInSolutionManager
+
+	e.LastHeartbeatTime = time.Now().UTC()
+
+	err = p.ExtensionManager.Register(p.Context, e)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Unregister removes an extension from the target
+func (p *Pluginer) Unregister(key string) error {
+	defer trace.End(trace.Begin(""))
+	if !p.connected {
+		return errors.New("not connected")
+	}
+
+	if err := p.ExtensionManager.Unregister(p.Context, key); err != nil {
+		return err
+	}
+	return nil
+}
+
+// IsRegistered checks for presence of an extension on the target
+func (p *Pluginer) IsRegistered(key string) (bool, error) {
+	defer trace.End(trace.Begin(""))
+	if !p.connected {
+		return false, errors.New("not connected")
+	}
+
+	l, err := p.Extensions()
+	if err != nil {
+		return false, err
+	}
+	for _, e := range l {
+		if e.Key == key {
+			log.Debugf("%s is registered", key)
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// Extensions lists all extensions present on the target
+func (p *Pluginer) Extensions() ([]types.Extension, error) {
+	defer trace.End(trace.Begin(""))
+	if !p.connected {
+		return nil, errors.New("not connected")
+	}
+	list, err := p.ExtensionManager.List(p.Context)
+
+	if err != nil {
+		return nil, err
+	}
+	return list, nil
+}
