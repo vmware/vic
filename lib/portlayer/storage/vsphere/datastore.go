64a127d71a78d09fddc9b52c0e013b9ab7af4aaa
Ian Whitlock <iwhitlock@vmware.com>
2016-07-15 16:33:32 -0500

    Add more context to traces

--- lib/portlayer/storage/vsphere/datastore.go
+++ lib/portlayer/storage/vsphere/datastore.go
@@ -312,7 +312,7 @@ var pathFormat = regexp.MustCompile(`\s([\/\w]+$)`)
 
 // Converts `[datastore] /path` to URL
 func DatastoreToURL(ds string) (*url.URL, error) {
-	defer trace.End(trace.Begin(""))
+	defer trace.End(trace.Begin(ds))
 	u := new(url.URL)
 	var matches []string
 	if matches = datastoreFormat.FindStringSubmatch(ds); len(matches) != 2 {
@@ -331,7 +331,7 @@ func DatastoreToURL(ds string) (*url.URL, error) {
 
 // Converts URL for datastores to datastore format ([datastore1] /path/to/thing)
 func URLtoDatastore(u *url.URL) (string, error) {
-	defer trace.End(trace.Begin(""))
+	defer trace.End(trace.Begin(u.String()))
 	scheme := "ds"
 	if u.Scheme != scheme {
 		return "", fmt.Errorf("url (%s) is not a datastore", u.String())
