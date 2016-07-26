98378fc23eae0fa9f103b71ea34aa3be944e35b1
Faiyaz Ahmed <fdawg4l@users.noreply.github.com>
2016-07-26 10:44:03 -0700

    Fix docker daemon volumes unit test and persist docker volumes metadata (#1664)
    * Fix PL REST API unit test
    
    The test wasn't running and had some copilation issues.  Refactored to
    PASS.
    
    Also, pass by ref vs pass by val issues needed some sorting.  This could
    probably use a second pass, but it's functional as is.
    
    * Persist volume metadata
    
    We weren't persisting the volume metadata the docker daemon was setting.
    We would cache it in the caching layer, but this wouldn't survive
    rebootrs.  This change will serialize the metadata and persist it to the
    `metadata` directory.  Likewise, on List(), we rehydrate this metadata
    when populating the cache.

--- lib/apiservers/engine/backends/volume.go
+++ lib/apiservers/engine/backends/volume.go
@@ -29,25 +29,27 @@ import (
 	"github.com/vmware/vic/pkg/trace"
 )
 
-//NOTE: FIXME: These might be moved to a utility package once there are multiple personalities
+// NOTE: FIXME: These might be moved to a utility package once there are multiple personalities
 const (
 	OptsVolumeStoreKey     string = "VolumeStore"
 	OptsCapacityKey        string = "Capacity"
 	dockerMetadataModelKey string = "DockerMetaData"
 )
 
-//Volume : struct which defines the docker personalities view of a Volume
+func NewVolumeModel(volume *models.VolumeResponse, labels map[string]string) *types.Volume {
+	return &types.Volume{
+		Driver:     volume.Driver,
+		Name:       volume.Name,
+		Labels:     labels,
+		Mountpoint: volume.Label,
+	}
+}
+
+// Volume which defines the docker personalities view of a Volume
 type Volume struct {
 }
 
-type volumeMetadata struct {
-	Driver     string
-	DriverOpts map[string]string
-	Name       string
-	Labels     map[string]string
-}
-
-//Volumes docker personality implementation for VIC
+// Volumes docker personality implementation for VIC
 func (v *Volume) Volumes(filter string) ([]*types.Volume, []string, error) {
 	defer trace.End(trace.Begin("Volume.Volumes"))
 	var volumes []*types.Volume
@@ -71,48 +73,51 @@ func (v *Volume) Volumes(filter string) ([]*types.Volume, []string, error) {
 
 	volumeResponses := res.Payload
 
-	log.Infof("volumes being returend : %+v", volumeResponses)
-	for i := range volumeResponses {
-		volumeMetadata := extractDockerMetadata(volumeResponses[i].Metadata)
-		volume := fillDockerVolumeModel(volumeResponses[i], volumeMetadata.Labels)
+	log.Infoln("volumes found: ")
+	for _, vol := range volumeResponses {
+		log.Infof("%s", vol.Name)
+		volumeMetadata, err := extractDockerMetadata(vol.Metadata)
+		if err != nil {
+			return nil, nil, fmt.Errorf("error unmarshalling docker metadata: %s", err)
+		}
+		volume := NewVolumeModel(vol, volumeMetadata.Labels)
 		volumes = append(volumes, volume)
 	}
-	log.Infof("volumes being returend : %+v", volumes)
 	return volumes, nil, nil
 }
 
-//VolumeInspect : docker personality implementation for VIC
+// VolumeInspect : docker personality implementation for VIC
 func (v *Volume) VolumeInspect(name string) (*types.Volume, error) {
 	return nil, fmt.Errorf("%s does not implement volume.VolumeInspect", ProductName())
 }
 
-//VolumeCreate : docker personality implementation for VIC
+// VolumeCreate : docker personality implementation for VIC
 func (v *Volume) VolumeCreate(name, driverName string, opts, labels map[string]string) (*types.Volume, error) {
 	defer trace.End(trace.Begin("Volume.VolumeCreate"))
 	result := &types.Volume{}
 
-	//TODO: design a way to have better error returns.
+	// TODO: design a way to have better error returns.
 
 	client := PortLayerClient()
 	if client == nil {
 		return nil, derr.NewErrorWithStatusCode(fmt.Errorf("Failed to get a portlayer client"), http.StatusInternalServerError)
 	}
 
-	//TODO: support having another driver besides vsphere.
-	//assign the values of the model to be passed to the portlayer handler
+	// TODO: support having another driver besides vsphere.
+	// assign the values of the model to be passed to the portlayer handler
 	model, varErr := translateInputsToPortlayerRequestModel(name, driverName, opts, labels)
 	if varErr != nil {
 		return result, derr.NewErrorWithStatusCode(fmt.Errorf("Bad Driver Arg: %s", varErr), http.StatusBadRequest)
 	}
 
-	//TODO: setup name randomization if name == nil
+	// TODO: setup name randomization if name == nil
 
-	res, err := client.Storage.CreateVolume(storage.NewCreateVolumeParams().WithVolumeRequest(&model))
+	res, err := client.Storage.CreateVolume(storage.NewCreateVolumeParams().WithVolumeRequest(model))
 	if err != nil {
 		switch err := err.(type) {
 
 		case *storage.CreateVolumeInternalServerError:
-			//FIXME: right now this does not return an error model...
+			// FIXME: right now this does not return an error model...
 			return result, derr.NewErrorWithStatusCode(fmt.Errorf("Server error from Portlayer: %s", err.Error()), http.StatusInternalServerError)
 
 		case *storage.CreateVolumeDefault:
@@ -123,11 +128,11 @@ func (v *Volume) VolumeCreate(name, driverName string, opts, labels map[string]s
 		}
 	}
 
-	result = fillDockerVolumeModel(res.Payload, labels)
+	result = NewVolumeModel(res.Payload, labels)
 	return result, nil
 }
 
-//VolumeRm : docker personality for VIC
+// VolumeRm : docker personality for VIC
 func (v *Volume) VolumeRm(name string) error {
 	defer trace.End(trace.Begin("Volume.VolumeRm"))
 
@@ -136,7 +141,7 @@ func (v *Volume) VolumeRm(name string) error {
 		return derr.NewErrorWithStatusCode(fmt.Errorf("Failed to get a portlayer client"), http.StatusInternalServerError)
 	}
 
-	//FIXME: check whether this is a name or a UUID. UUID expected for now.
+	// FIXME: check whether this is a name or a UUID. UUID expected for now.
 	_, err := client.Storage.RemoveVolume(storage.NewRemoveVolumeParams().WithName(name))
 	if err != nil {
 
@@ -156,32 +161,55 @@ func (v *Volume) VolumeRm(name string) error {
 	return nil
 }
 
-//Utility Functions
-
-func fillDockerVolumeModel(volume *models.VolumeResponse, labels map[string]string) *types.Volume {
-	dockerVol := types.Volume{
-		Driver:     volume.Driver,
-		Name:       volume.Name,
-		Labels:     labels,
-		Mountpoint: volume.Label,
-	}
-	return &dockerVol
+type volumeMetadata struct {
+	Driver     string
+	DriverOpts map[string]string
+	Name       string
+	Labels     map[string]string
 }
 
+func createVolumeMetadata(model *models.VolumeRequest, labels map[string]string) (string, error) {
+	metadata := volumeMetadata{
+		Driver:     model.Driver,
+		DriverOpts: model.DriverArgs,
+		Name:       model.Name,
+		Labels:     labels,
+	}
+	result, err := json.Marshal(metadata)
+	return string(result), err
+}
+
+// Unmarshal the docker metadata using the docker metadata key.  The docker
+// metadatakey.  We stash the vals we know about in that map with that key.
+func extractDockerMetadata(metadataMap map[string]string) (*volumeMetadata,
+	error) {
+	v, ok := metadataMap[dockerMetadataModelKey]
+	if !ok {
+		return nil, nil
+	}
+
+	result := &volumeMetadata{}
+	err := json.Unmarshal([]byte(v), result)
+	return result, err
+}
+
+// Utility Functions
+
 func validateDriverArgs(args map[string]string, model *models.VolumeRequest) error {
-	//volumestore name validation
+	// volumestore name validation
 	storeName, ok := args[OptsVolumeStoreKey]
 	if !ok {
 		storeName = "default"
 	}
 	model.Store = storeName
 
-	//capacity validation
+	// capacity validation
 	capstr, ok := args[OptsCapacityKey]
 	if !ok {
 		model.Capacity = -1
 		return nil
 	}
+
 	capacity, convErr := strconv.ParseInt(capstr, 10, 64)
 	if convErr != nil {
 		model.Capacity = -1
@@ -191,34 +219,23 @@ func validateDriverArgs(args map[string]string, model *models.VolumeRequest) err
 	return nil
 }
 
-func translateInputsToPortlayerRequestModel(name, driverName string, opts, labels map[string]string) (models.VolumeRequest, error) {
-	model := models.VolumeRequest{
+func translateInputsToPortlayerRequestModel(name, driverName string, opts, labels map[string]string) (*models.VolumeRequest, error) {
+	model := &models.VolumeRequest{
 		Driver:     driverName,
 		DriverArgs: opts,
 		Name:       name,
 	}
-	metadata := createVolumeMetadata(&model, labels)
+
+	metadata, err := createVolumeMetadata(model, labels)
+	if err != nil {
+		return nil, err
+	}
+
 	model.Metadata = make(map[string]string)
 	model.Metadata[dockerMetadataModelKey] = metadata
-	if err := validateDriverArgs(opts, &model); err != nil {
-		return model, err
+	if err := validateDriverArgs(opts, model); err != nil {
+		return nil, err
 	}
+
 	return model, nil
-}
-
-func createVolumeMetadata(model *models.VolumeRequest, labels map[string]string) string {
-	metadata := volumeMetadata{
-		Driver:     model.Driver,
-		DriverOpts: model.DriverArgs,
-		Name:       model.Name,
-		Labels:     labels,
-	}
-	result, _ := json.Marshal(metadata)
-	return string(result)
-}
-
-func extractDockerMetadata(metadataMap map[string]string) volumeMetadata {
-	var result volumeMetadata
-	json.Unmarshal([]byte(metadataMap[dockerMetadataModelKey]), result)
-	return result
 }
