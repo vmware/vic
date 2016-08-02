bcf88eb02203f6fe4e7393bfe98b0a62f6d92cf0
Anchal Agrawal <anchala@vmware.com>
2016-08-02 16:14:48 -0500

    Fix network rm using id and enable integ test

--- lib/portlayer/network/context.go
+++ lib/portlayer/network/context.go
@@ -984,15 +984,18 @@ func (c *Context) DeleteScope(name string) error {
 
 	// remove gateway ip from bridge interface
 	addr := net.IPNet{IP: s.Gateway(), Mask: s.Subnet().Mask}
-	if err = Config.BridgeLink.AddrDel(addr); err != nil {
-		if errno, ok := err.(syscall.Errno); !ok || errno != syscall.EADDRNOTAVAIL {
-			log.Warnf("could not remove gateway address %s for scope %s on link %s: %s", addr, s.Name(), Config.BridgeLink.Attrs().Name, err)
-		}
 
-		err = nil
+	if s.Type() != BridgeScopeType {
+		if err = Config.BridgeLink.AddrDel(addr); err != nil {
+			if errno, ok := err.(syscall.Errno); !ok || errno != syscall.EADDRNOTAVAIL {
+				log.Warnf("could not remove gateway address %s for scope %s on link %s: %s", addr, s.Name(), Config.BridgeLink.Attrs().Name, err)
+			}
+
+			err = nil
+		}
 	}
 
-	delete(c.scopes, name)
+	delete(c.scopes, s.Name())
 	return nil
 }
 
