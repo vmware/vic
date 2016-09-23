ab8a00f0c8f1f37524eb8e4b9e5aa9b08deb2b3a
Hasan Mahmood <mahmoodh@vmware.com>
2016-09-23 11:16:52 -0500

    Not persisting network info in guestinfo

--- lib/portlayer/network/context.go
+++ lib/portlayer/network/context.go
@@ -437,22 +437,6 @@ func (c *Context) NewScope(scopeType, name string, subnet *net.IPNet, gateway ne
 		return nil, err
 	}
 
-	// add the new scope to the config
-	c.config.ContainerNetworks[s.Name()] = &ContainerNetwork{
-		Common: executor.Common{
-			ID:   s.ID().String(),
-			Name: s.Name(),
-		},
-		Type:        s.Type(),
-		Gateway:     net.IPNet{IP: s.Gateway(), Mask: s.Subnet().Mask},
-		Nameservers: s.DNS(),
-		Pools:       s.IPAM().Pools(),
-		PortGroup:   s.network,
-	}
-
-	// write config
-	c.config.Encode()
-
 	return s, nil
 }
 
@@ -917,6 +901,7 @@ func (c *Context) AddContainer(h *exec.Handle, options *AddContainerOptions) err
 			},
 			Aliases: options.Aliases,
 			Pools:   s.IPAM().Pools(),
+			Type:    s.Type(),
 		},
 		Ports: options.Ports,
 	}
