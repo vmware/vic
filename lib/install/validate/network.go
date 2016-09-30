2f390a0391afd6d203a5049015c6c00825edd73c
Hasan Mahmood <mahmoodh@vmware.com>
2016-09-29 22:04:39 -0500

    Add external network to list of builtin networks in the port layer

--- lib/install/validate/network.go
+++ lib/install/validate/network.go
@@ -28,6 +28,7 @@ import (
 	"github.com/vmware/vic/lib/config"
 	"github.com/vmware/vic/lib/config/executor"
 	"github.com/vmware/vic/lib/install/data"
+	"github.com/vmware/vic/lib/portlayer/constants"
 	"github.com/vmware/vic/pkg/errors"
 	"github.com/vmware/vic/pkg/ip"
 	"github.com/vmware/vic/pkg/trace"
@@ -151,7 +152,7 @@ func (v *Validator) network(ctx context.Context, input *data.Data, conf *config.
 				Name: "bridge",
 				ID:   netMoid,
 			},
-			Type: "bridge",
+			Type: constants.BridgeScopeType,
 		},
 	}
 	// we need to have the bridge network identified as an available container network
@@ -160,6 +161,10 @@ func (v *Validator) network(ctx context.Context, input *data.Data, conf *config.
 	// port forwarding
 	conf.AddNetwork(bridgeNet)
 	conf.BridgeIPRange = input.BridgeIPRange
+
+	externalNet := conf.Networks["external"].Network
+	externalNet.Type = constants.ExternalScopeType
+	conf.AddContainerNetwork(&externalNet)
 
 	err = v.checkVDSMembership(ctx, endpointMoref, input.BridgeNetworkName)
 	if err != nil && checkBridgeVDS {
@@ -230,7 +235,7 @@ func (v *Validator) network(ctx context.Context, input *data.Data, conf *config.
 				Name: name,
 				ID:   moref.String(),
 			},
-			Type:        "external",
+			Type:        constants.ExternalScopeType,
 			Gateway:     gw,
 			Nameservers: dns,
 			Pools:       pools,
