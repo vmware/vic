884294d611fa46867d275a4860bd5a63ac0dd1d0
Hasan Mahmood <mahmoodh@vmware.com>
2016-09-30 18:18:30 -0500

    Revert "Add external network to list of builtin networks in the port layer"
    This reverts commit b137b142d641c78af585b86dd512aa00a17a9ef5.

--- lib/install/validate/network.go
+++ lib/install/validate/network.go
@@ -28,7 +28,6 @@ import (
 	"github.com/vmware/vic/lib/config"
 	"github.com/vmware/vic/lib/config/executor"
 	"github.com/vmware/vic/lib/install/data"
-	"github.com/vmware/vic/lib/portlayer/constants"
 	"github.com/vmware/vic/pkg/errors"
 	"github.com/vmware/vic/pkg/ip"
 	"github.com/vmware/vic/pkg/trace"
@@ -152,7 +151,7 @@ func (v *Validator) network(ctx context.Context, input *data.Data, conf *config.
 				Name: "bridge",
 				ID:   netMoid,
 			},
-			Type: constants.BridgeScopeType,
+			Type: "bridge",
 		},
 	}
 	// we need to have the bridge network identified as an available container network
@@ -161,10 +160,6 @@ func (v *Validator) network(ctx context.Context, input *data.Data, conf *config.
 	// port forwarding
 	conf.AddNetwork(bridgeNet)
 	conf.BridgeIPRange = input.BridgeIPRange
-
-	externalNet := conf.Networks["external"].Network
-	externalNet.Type = constants.ExternalScopeType
-	conf.AddContainerNetwork(&externalNet)
 
 	err = v.checkVDSMembership(ctx, endpointMoref, input.BridgeNetworkName)
 	if err != nil && checkBridgeVDS {
@@ -235,7 +230,7 @@ func (v *Validator) network(ctx context.Context, input *data.Data, conf *config.
 				Name: name,
 				ID:   moref.String(),
 			},
-			Type:        constants.ExternalScopeType,
+			Type:        "external",
 			Gateway:     gw,
 			Nameservers: dns,
 			Pools:       pools,
