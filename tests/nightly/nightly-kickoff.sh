010e720ce03356369faa88851f7ee37e802548bc
Ashok Rajan <rashok@vmware.com>
2016-10-07 17:41:47 -0500

    Collecting container logs for functional tests

--- tests/nightly/nightly-kickoff.sh
+++ tests/nightly/nightly-kickoff.sh
@@ -15,7 +15,7 @@
 #
 
 echo "Removing VIC directory if present"
-rm -rf vic
+rm -rf bin
 
 echo "Cleanup logs from previous run"
 rm -f *.xml 
@@ -28,8 +28,10 @@ input=$(wget -O - https://vmware.bintray.com/vic-repo |tail -n5 |head -n1 |cut -
 echo "Downloading bintray file"
 wget https://vmware.bintray.com/vic-repo/$input.tar.gz
 
+mkdir bin
+
 echo "Extracting .tar.gz"
-tar xzf $input.tar.gz
+tar xvzf $input.tar.gz -C bin/ --strip 1
 
 echo "Deleting .tar.gz vic file"
 rm $input.tar.gz
@@ -38,40 +40,42 @@ drone exec --trusted -e test="pybot tests/manual-test-cases/Group5-Functional-Te
 
 mv output.xml output_5-1-Distributed-Switch.xml
 mv log.html log_5-1-Distributed-Switch.html
+mv vic-machine.log vic-machine_5-1-Distributed-Switch.log
 
 drone exec --trusted -e test="pybot tests/manual-test-cases/Group5-Functional-Tests/5-2-Cluster.robot" -E nightly_test_secrets.yml --yaml .drone.nightly.yml
 
 mv output.xml output_5-2-Cluster.xml
 mv log.html log_5-2-Cluster.html
+mv vic-machine.log vic-machine_5-2-Cluster.log
 
 drone exec --trusted -e test="pybot tests/manual-test-cases/Group5-Functional-Tests/5-4-High-Availability.robot" -E nightly_test_secrets.yml --yaml .drone.nightly.yml
 
 mv output.xml output_5-4-High-Availability.xml
 mv log.html log_5-4-High-Availability.html
+mv vic-machine.log vic-machine_5-4-High-Availability.log
 
 drone exec --trusted -e test="pybot tests/manual-test-cases/Group5-Functional-Tests/5-5-Heterogenous-ESXi.robot" -E nightly_test_secrets.yml --yaml .drone.nightly.yml
 
 mv output.xml output_5-5-Heterogenous-ESXi.xml
 mv log.html log_5-5-Heterogenous-ESXi.html
+mv vic-machine.log vic-machine_5-5-Heterogenous-ESXi.log
 
 drone exec --trusted -e test="pybot tests/manual-test-cases/Group5-Functional-Tests/5-6-VSAN.robot" -E nightly_test_secrets.yml --yaml .drone.nightly.yml
 
 mv output.xml output_5-6-VSAN.xml
 mv log.html log_5-6-VSAN.html
+mv vic-machine.log vic-machine_5-6-VSAN.log
 
 drone exec --trusted -e test="pybot tests/manual-test-cases/Group5-Functional-Tests/5-7-NSX.robot" -E nightly_test_secrets.yml --yaml .drone.nightly.yml
 
 mv output.xml output_5-7-NSX.xml
 mv log.html log_5-7-NSX.html
+mv vic-machine.log vic-machine_5-7-NSX.log
 
 drone exec --trusted -e test="pybot tests/manual-test-cases/Group5-Functional-Tests/5-8-DRS.robot" -E nightly_test_secrets.yml --yaml .drone.nightly.yml
 
 mv output.xml output_5-8-DRS.xml
 mv log.html log_5-8-DRS.html
-
-drone exec --trusted -e test="pybot tests/manual-test-cases/Group5-Functional-Tests/5-9-Private-Registry.robot" -E nightly_test_secrets.yml --yaml .drone.nightly.yml
-
-mv output.xml output_5-9-Private-Registry.xml
-mv log.html log_5-9-Private-Registry.html
+mv vic-machine.log vic-machine_5-8-DRS.log
 
 drone exec --trusted -e test="sh tests/nightly/upload-logs.sh $input" -E nightly_test_secrets.yml --yaml .drone.nightly.yml
