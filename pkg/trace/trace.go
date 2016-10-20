501b914939d5225ecfa1938f5894eb1b51d38ec1
Volodymyr Burenin <vburenin@gmail.com>
2016-10-20 13:42:16 -0500

    Check log level befor initializing tracer.

--- pkg/trace/trace.go
+++ pkg/trace/trace.go
@@ -82,17 +82,16 @@ func newTrace(msg string, skip int) *Message {
 
 // Begin starts the trace.  Msg is the msg to log.
 func Begin(msg string) *Message {
-	if !tracingEnabled {
-		return nil
-	}
-	if t := newTrace(msg, 2); t != nil {
-		if msg == "" {
-			Logger.Debugf("[BEGIN] [%s:%d]", t.funcName, t.lineNo)
-		} else {
-			Logger.Debugf("[BEGIN] [%s:%d] %s", t.funcName, t.lineNo, t.msg)
-		}
-		return t
+	if !tracingEnabled && Logger.Level >= logrus.DebugLevel {
+		if t := newTrace(msg, 2); t != nil {
+			if msg == "" {
+				Logger.Debugf("[BEGIN] [%s:%d]", t.funcName, t.lineNo)
+			} else {
+				Logger.Debugf("[BEGIN] [%s:%d] %s", t.funcName, t.lineNo, t.msg)
+			}
+			return t
 
+		}
 	}
 	return nil
 }
