710a58ddbe15aee834dbf76188f7abc23367c521
chengwang86 <cheng.wang1986@gmail.com>
2016-10-31 08:39:39 -0700

    remove comment

--- pkg/vsphere/session/session.go
+++ pkg/vsphere/session/session.go
@@ -205,7 +205,6 @@ func (s *Session) Connect(ctx context.Context) (*Session, error) {
 	}
 
 	if s.Keepalive != 0 {
-		// TODO: add login() to the keep alive handler
 		vimClient.RoundTripper = session.KeepAliveHandler(soapClient, s.Keepalive,
 			func(roundTripper soap.RoundTripper) error {
 				_, err := methods.GetCurrentTime(context.Background(), roundTripper)
