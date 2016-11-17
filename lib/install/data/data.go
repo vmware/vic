18ce8aa89e608d802be43d187122864ee9cee603
sflxn <nloc@vmware.com>
2016-11-17 13:36:29 -0800

    Add support for self generated CAs for registries
    Add support for uploading certificate authority certs via vic-machine's
    "--registry-ca" for docker registry login and pull usage.  The VCH's
    default CA bundle will not be affected.  Any other application running
    in the VCH will not be affected.
    
    The docker personality will use these CA certs for docker login and
    docker pull.
    
    Resolves #2103

--- lib/install/data/data.go
+++ lib/install/data/data.go
@@ -32,9 +32,10 @@ type Data struct {
 	common.Compute
 	common.VCHID
 
-	CertPEM   []byte
-	KeyPEM    []byte
-	ClientCAs []byte
+	CertPEM   	[]byte
+	KeyPEM    	[]byte
+	ClientCAs 	[]byte
+	RegistryCAs []byte
 	common.Images
 
 	ImageDatastorePath     string
