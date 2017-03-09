c27d0d30ba842564cb286751fe4fd14e3c088c79
Emma <line@vmware.com>
2017-03-09 11:54:47 -0600

    support link in user defined network

--- lib/apiservers/engine/backends/container_proxy.go
+++ lib/apiservers/engine/backends/container_proxy.go
@@ -908,7 +908,7 @@ func toModelsNetworkConfig(cc types.ContainerCreateConfig) *models.NetworkConfig
 	}
 
 	// Docker copies Links to NetworkConfig only if it is a UserDefined network, handle that
-	if !cc.HostConfig.NetworkMode.IsUserDefined() && len(cc.HostConfig.Links) > 0 {
+	if cc.HostConfig.NetworkMode.IsUserDefined() && len(cc.HostConfig.Links) > 0 {
 		nc.Aliases = append(nc.Aliases, cc.HostConfig.Links...)
 	}
 
