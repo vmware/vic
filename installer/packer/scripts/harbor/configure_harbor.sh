677d68fbb6b056c7e7ebac8ae555098fc3f294f2
Fabio Rapposelli <fabio@vmware.com>
2017-04-07 16:08:30 +0200

    Set umask 077 when creating sensitive files
    (cherry picked from commit b977b19ab64b7fe8700dd11fa6e4bf05ac9683dc)

--- installer/packer/scripts/harbor/configure_harbor.sh
+++ installer/packer/scripts/harbor/configure_harbor.sh
@@ -12,7 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -x -euf -o pipefail
+set -euf -o pipefail
+
+umask 077
 
 deploy=$(ovfenv -k registry.deploy)
 
