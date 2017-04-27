4fa781035e2aefd46f6cb8e7c377ddff990f6845
Josh Kim <kjosh@vmware.com>
2017-04-27 15:28:06 -0500

    Consolidate UI installer for VCSA
    This resolves the issue number 4906 and consolidates multiple
    install.sh files into just VCSA/installer.sh. User should be able to
    install the Flex client plugin by modifying the PLUGIN_TYPE variable
    in the configs file

--- ui/installer/VCSA/uninstall.sh
+++ ui/installer/VCSA/uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2016 VMware, Inc. All Rights Reserved.
+# Copyright 2017 VMware, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,21 +14,35 @@
 # limitations under the License.
 #
 
+# check for the configs file
 if [[ ! -f "configs" ]] ; then
     echo "Error! Configs file is missing. Please try downloading the VIC UI installer again"
     echo ""
     exit 1
 fi
 
-CONFIGS_FILE="configs"
+# load configs variable into env
 while IFS='' read -r line; do
     eval $line
-done < $CONFIGS_FILE
+done < ./configs
 
+# check for the VC IP
 if [[ $VCENTER_IP == "" ]] ; then
     echo "Error! vCenter IP cannot be empty. Please provide a valid IP in the configs file"
     exit 1
 fi
+
+# check for the pllugin manifest file
+if [[ ! -f ../plugin-manifest ]] ; then
+    echo "Error! Plugin manifest was not found!"
+    cleanup
+    exit 1
+fi
+
+# load plugin manifest into env
+while IFS='' read -r p_line; do
+    eval "$p_line"
+done < ../plugin-manifest
 
 read -p "Enter your vCenter Administrator Username: " VCENTER_ADMIN_USERNAME
 echo -n "Enter your vCenter Administrator Password: "
@@ -48,9 +62,13 @@ else
 fi
 
 check_prerequisite () {
-    if [[ ! -d ../vsphere-client-serenity ]] ; then
-        echo "Error! VIC UI plugin bundle was not found. Please try downloading the VIC UI installer again"
-        exit 1
+    # if PLUGIN_TYPE is not specified default to html5 plugin
+    if [[ $PLUGIN_TYPE = 'flex' ]] ; then
+        PLUGIN_TYPE=flex
+        key=$key_flex
+    else
+        PLUGIN_TYPE=html5
+        key=$key_h5c
     fi
 
     if [[ $(curl -v --head https://$VCENTER_IP -k 2>&1 | grep -i "could not resolve host") ]] ; then
@@ -60,33 +78,15 @@ check_prerequisite () {
 }
 
 parse_and_unregister_plugins () {
-    for d in ../vsphere-client-serenity/* ; do
-        if [[ -d $d ]] ; then
-            echo "Reading plugin-package.xml..."
+    local plugin_flags="--key $key"
+    echo "-------------------------------------------------------------"
+    echo "Unregistering vCenter Server Extension..."
+    echo "-------------------------------------------------------------"
+    $PLUGIN_MANAGER_BIN remove $COMMONFLAGS $plugin_flags
 
-            while IFS='' read -r p_line; do
-                eval "local $p_line"
-            done < $d/vc_extension_flags
-
-            local plugin_flags="--key $key"
-            echo "-------------------------------------------------------------"
-            echo "Unregistering vCenter Server Extension..."
-            echo "-------------------------------------------------------------"
-            $PLUGIN_MANAGER_BIN remove $COMMONFLAGS $plugin_flags
-
-            if [[ $? > 0 ]] ; then
-                echo "-------------------------------------------------------------"
-                echo "Error! Could not unregister plugin from vCenter Server. Please see the message above."
-                exit 1
-            fi
-        fi
-    done
-}
-
-rename_package_folder () {
-    mv $1 $2
     if [[ $? > 0 ]] ; then
-        echo "Error! Could not rename folder"
+        echo "-------------------------------------------------------------"
+        echo "Error! Could not unregister plugin from vCenter Server. Please see the message above."
         exit 1
     fi
 }
