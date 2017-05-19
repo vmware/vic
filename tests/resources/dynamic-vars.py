e915b138f40475437e5003ed29b4e7e8f8df0a0f
Mikkel Hagen <mhagen@vmware.com>
2017-05-19 15:27:10 -0500

    Make sure it is a string

--- tests/resources/dynamic-vars.py
+++ tests/resources/dynamic-vars.py
@@ -1,3 +1,3 @@
 import os
 
-BUSYBOX = harbor.ci.drone.local/library/busybox if (os.environ.has_key("DRONE_BUILD_NUMBER") and (int(os.environ['DRONE_BUILD_NUMBER']) != 0)) else busybox
+BUSYBOX = 'harbor.ci.drone.local/library/busybox' if (os.environ.has_key("DRONE_BUILD_NUMBER") and (int(os.environ['DRONE_BUILD_NUMBER']) != 0)) else 'busybox'
