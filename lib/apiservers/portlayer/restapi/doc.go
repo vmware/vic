657d337717d5ae6ac063437ee4a01028a2927b01
Jake Dsouza <jaked@vmware.com>
2017-05-19 16:52:11 -0700

    Venfor portlayer client and server

---
+++ lib/apiservers/portlayer/restapi/doc.go
@@ -0,0 +1,28 @@
+/*Package restapi Port Layer API
+
+Port Layer API
+
+
+    Schemes:
+      http
+    Host: localhost
+    BasePath: /
+    Version: v0.0.1
+
+    Consumes:
+    - application/json
+
+    - application/raw-stream
+
+
+    Produces:
+    - application/json
+
+    - application/octet-stream
+
+    - text/plain
+
+
+swagger:meta
+*/
+package restapi
