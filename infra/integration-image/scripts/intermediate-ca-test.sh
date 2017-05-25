98186c1e47044009ff432f5b8e328a5158c7b50b
Hasan Mahmood <mahmoodh@vmware.com>
2017-05-25 12:41:45 -0500

    Removing redundant lines

--- infra/integration-image/scripts/intermediate-ca-test.sh
+++ infra/integration-image/scripts/intermediate-ca-test.sh
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#!/usr/bin/env bash
 
 # Generate a root CA, intermediate CA, and a server certificate
 
--- infra/integration-image/scripts/ubuntu-remove-intermediate-ca.sh
+++ infra/integration-image/scripts/ubuntu-remove-intermediate-ca.sh
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#!/usr/bin/env bash
 
 rm /usr/local/share/ca-certificates/intermediate.cert.crt
 
