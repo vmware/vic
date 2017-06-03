e43b7eba655ef184e042521877af4859a5687793
Hasan Mahmood <mahmoodh@vmware.com>
2017-06-03 18:59:48 -0500

    Taking away URL case and domainEntry

--- pkg/registry/entry.go
+++ pkg/registry/entry.go
@@ -188,6 +188,6 @@ func parseURL(s string) *url.URL {
 	return nil
 }
 
-func (w *domainEntry) Equal(other Entry) bool {
+func (w *strEntry) Equal(other Entry) bool {
 	return other.String() == w.String()
 }
