91738f0d221a15708866a6a435097fe8e665a584
Josh Kim <kjosh@vmware.com>
2017-06-09 18:07:54 -0500

    Upgrade Angular and Clarity
    Upgraded Angular 2.4.10 to the latest Angular 4 by using the
    Angular CLI. Also upgraded the Clarity to the latest.

--- ui/vic-ui-h5c/vic/src/vic-webapp/protractor.conf.js
+++ ui/vic-ui-h5c/vic/src/vic-webapp/protractor.conf.js
@@ -17,7 +17,9 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const {
+  SpecReporter
+} = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -33,12 +35,16 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {}
   },
   onPrepare() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(new SpecReporter({
+      spec: {
+        displayStacktrace: true
+      }
+    }));
   }
 };
