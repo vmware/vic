11cf484e3622a871b0779d6729294458e6fbac17
Mikkel Hagen <mhagen@vmware.com>
2017-07-18 09:40:18 -0500

    Docker push integration tests

--- tests/resources/dynamic-vars.py
+++ tests/resources/dynamic-vars.py
@@ -3,6 +3,7 @@ import os
 IS_LOCAL = False if (os.environ.has_key("DRONE_BUILD_NUMBER") and (int(os.environ['DRONE_BUILD_NUMBER']) != 0)) else True
 
 BUSYBOX =  'busybox' if IS_LOCAL else 'harbor.ci.drone.local/library/busybox'
+BUSYBOX_1_26_0 =  'busybox:1.26.0' if IS_LOCAL else 'harbor.ci.drone.local/library/busybox:1.26.0'
 ALPINE =  'alpine' if IS_LOCAL else 'harbor.ci.drone.local/library/alpine'
 NGINX =  'nginx' if IS_LOCAL else 'harbor.ci.drone.local/library/nginx'
 DEBIAN =  'debian' if IS_LOCAL else 'harbor.ci.drone.local/library/debian'
