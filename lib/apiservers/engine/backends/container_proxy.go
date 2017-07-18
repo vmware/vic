dde6cbf84d41066c09f88be17e7de72bf7b2ac19
Emma <line@vmware.com>
2017-07-18 17:33:58 -0500

    merge with master

--- lib/apiservers/engine/backends/container_proxy.go
+++ lib/apiservers/engine/backends/container_proxy.go
@@ -717,9 +717,7 @@ func (c *ContainerProxy) ArchiveExportReader(ctx context.Context, store, ancesto
 			// stream.  The other way is for the caller of this function to close the returned CloseReader.
 			// Callers of this function should do one but not both.
 			pipeReader.Close()
-			transport.Close()
 		case <-done:
-			transport.Close()
 			pipeWriter.Close()
 		}
 	}()
