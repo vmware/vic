2ea724c8f1b0ab2c164ba58dac98034b11eb0a0b
angiecris <angiew@vmware.com>
2017-08-22 15:40:41 -0500

    removed unecessary asserts from unit test

--- pkg/fetcher/fetcher_test.go
+++ pkg/fetcher/fetcher_test.go
@@ -19,11 +19,12 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"fmt"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	ErrJsonStr401 = `{
+	errJsonStr401 = `{
 	"errors":
 		[{"code":"UNAUTHORIZED",
 		  "message":"authentication required",
@@ -31,7 +32,7 @@ const (
 		}]
 	}
 	`
-	MultipleErrJsonStr = `{
+	multipleErrJsonStr = `{
 	"errors":
 		[{"code":"UNAUTHORIZED",
 		  "message":"authentication required",
@@ -43,68 +44,65 @@ const (
 		}]
 	}
 	`
-	RandomStr                    = `random`
-	RandomJsonStr                = `{"nope":"nope"}`
-	ErrJsonWithEmptyErrorsField  = `{"errors":[]}`
-	ErrJsonWithNoMessageField    = `{"errors":[{"code":"nope","detail":"nope"}]}`
-	ErrJsonWithEmptyMessageField = `{"errors":[{"code":"nope","message":""},{"message":""}]}`
+	unexpectedStr                = `random`
+	unexpectedJsonStr            = `{"nope":"nope"}`
+	errJsonWithEmptyErrorsField  = `{"errors":[]}`
+	errJsonWithNoMessageField    = `{"errors":[{"code":"nope","detail":"nope"}]}`
+	errJsonWithEmptyMessageField = `{"errors":[{"code":"nope","message":""},{"message":""}]}`
 )
 
 func TestExtractErrResponseMessage(t *testing.T) {
 	// Test set up: create the io streams for testing purposes
 	// multiple streams needed: these streams only have read ends
-	singleErrTestStream := ioutil.NopCloser(bytes.NewReader([]byte(ErrJsonStr401)))
-	multipleErrTestStream := ioutil.NopCloser(bytes.NewReader([]byte(MultipleErrJsonStr)))
-	randomStrTestStream := ioutil.NopCloser(bytes.NewReader([]byte(RandomStr)))
-	malformedJSONTestStream := ioutil.NopCloser(bytes.NewReader([]byte(RandomJsonStr)))
-	emptyErrorsJSONTestStream := ioutil.NopCloser(bytes.NewReader([]byte(ErrJsonWithEmptyErrorsField)))
-	noMessageJSONTestStream := ioutil.NopCloser(bytes.NewReader([]byte(ErrJsonWithNoMessageField)))
-	emptyMessageJSONTestStream := ioutil.NopCloser(bytes.NewReader([]byte(ErrJsonWithEmptyMessageField)))
+	singleErrTestStream := ioutil.NopCloser(bytes.NewReader([]byte(errJsonStr401)))
+	multipleErrTestStream := ioutil.NopCloser(bytes.NewReader([]byte(multipleErrJsonStr)))
+	unexpectedStrTestStream := ioutil.NopCloser(bytes.NewReader([]byte(unexpectedStr)))
+	malformedJSONTestStream := ioutil.NopCloser(bytes.NewReader([]byte(unexpectedJsonStr)))
+	emptyErrorsJSONTestStream := ioutil.NopCloser(bytes.NewReader([]byte(errJsonWithEmptyErrorsField)))
+	noMessageJSONTestStream := ioutil.NopCloser(bytes.NewReader([]byte(errJsonWithNoMessageField)))
+	emptyMessageJSONTestStream := ioutil.NopCloser(bytes.NewReader([]byte(errJsonWithEmptyMessageField)))
+
+	// expected error message upon malformed json to check against
+	malformedJsonErrFormat := fmt.Errorf("error response json has unconventional format")
 
 	// Test 1: single error message extraction
 	msg, err := extractErrResponseMessage(singleErrTestStream)
 	assert.Nil(t, err, "test: (single error message) extraction should success for well-formatted error json")
-	assert.NotNil(t, msg, "test: (single error message) message extracted for well-formatted error json")
 	assert.Equal(t, "authentication required", msg,
 		"test: (single error message) extracted message: %s; expected: authentication required", msg)
 
 	// Test 2: multiple error message extraction
 	msg, err = extractErrResponseMessage(multipleErrTestStream)
 	assert.Nil(t, err, "test: (multiple error messages) extraction should success for well-formatted error json")
-	assert.NotNil(t, msg, "test: (multiple error messages) message extracted for well-formatted error json")
 	assert.Equal(t, "authentication required, image not found", msg,
 		"test: (multiple error messages) extracted message: %s; expected: authentication required, image not found", msg)
 
 	// Test 3: random string in the stream that is not a json
-	msg, err = extractErrResponseMessage(randomStrTestStream)
+	msg, err = extractErrResponseMessage(unexpectedStrTestStream)
 	assert.Equal(t, "", msg, "test: (non-json string) no message should be extracted")
 	assert.NotNil(t, err, "test: (non-json string) extraction should fail")
 
 	// Test 4: malformed json string
 	msg, err = extractErrResponseMessage(malformedJSONTestStream)
 	assert.Equal(t, "", msg, "test: (malformed json string) no message should be extracted")
-	assert.NotNil(t, err, "test: (malformed json string) extraction should fail")
-	assert.Equal(t, "error response json has unconventional format", err.Error(),
-		"test: (malformed json string) error: error response json has unconventional format; expected error: %s", err.Error())
+	assert.Equal(t, malformedJsonErrFormat, err,
+		"test: (malformed json string) error: %s; expected error: %s", err, malformedJsonErrFormat)
 
 	// Test 5: malformed json with empty `errors` field
 	msg, err = extractErrResponseMessage(emptyErrorsJSONTestStream)
 	assert.Equal(t, "", msg, "test: (malformed json string, empty errors field) no message should be extracted")
-	assert.NotNil(t, err, "test: (malformed json string, empty errors field) extraction should fail")
-	assert.Equal(t, "error response json has unconventional format", err.Error(),
-		"test: (malformed json string, empty errors field) error: %s; expected error: error response json has unconventional format", err.Error())
+	assert.Equal(t, malformedJsonErrFormat, err,
+		"test: (malformed json string, empty errors field) error: %s; expected error: %s", err, malformedJsonErrFormat)
 
 	// Test 6: malformed json with no `message` field
 	msg, err = extractErrResponseMessage(noMessageJSONTestStream)
 	assert.Equal(t, "", msg, "test: (malformed json string, no message field) no message should be extracted")
-	assert.NotNil(t, err, "test: (malformed json string, no message field) extraction should fail")
-	assert.Equal(t, "error response json has unconventional format", err.Error(),
-		"test: (malformed json string, no message field) error: %s; expected error: error response json has unconventional format", err.Error())
+	assert.Equal(t, malformedJsonErrFormat, err,
+		"test: (malformed json string, no message field) error: %s; expected error: %s", err, malformedJsonErrFormat)
 
 	// Test 7: malformed json with empty string in `message` field
 	msg, err = extractErrResponseMessage(emptyMessageJSONTestStream)
 	assert.Equal(t, "", msg, "test: (malformed json string, empty message field) no message should be extracted")
-	assert.NotNil(t, err, "test: (malformed json string, empty message field) extraction should fail")
-	assert.Equal(t, "error response json has unconventional format", err.Error(),
-		"test: (malformed json string, empty message field) error: %s; expected error: error response json has unconventional format", err.Error())
+	assert.Equal(t, malformedJsonErrFormat, err,
+		"test: (malformed json string, empty message field) error: %s; expected error: %s", err, malformedJsonErrFormat)
 }
