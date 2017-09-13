a6d7f53767ed25ff4ec57ac6b2af681d42081005
Zach Shepherd <shepherdz@vmware.com>
2017-09-13 13:06:28 -0700

    Define REST API handlers for inspecting VCHs
    Introduce a pair of handlers for accessing information about the VCH
    with a given identifier within a vSphere target or named datacenter.
    
    There are ways in which this work is incomplete, marked by TODO:
     - We accept datacenters by name, not ID
     - The name of the VCH's compute resource is returned, not its ID
     - The names of portgroups used by the VCH are returned, not the IDs
     - Malformed or unexpected PKI material is ignored

---
+++ lib/apiservers/service/restapi/handlers/common.go
@@ -0,0 +1,124 @@
+// Copyright 2017 VMware, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/vim25/types"
+	"github.com/vmware/vic/cmd/vic-machine/common"
+	"github.com/vmware/vic/lib/apiservers/service/restapi/handlers/util"
+	"github.com/vmware/vic/lib/install/data"
+	"github.com/vmware/vic/lib/install/validate"
+	"github.com/vmware/vic/pkg/version"
+	"github.com/vmware/vic/pkg/vsphere/vm"
+)
+
+func buildData(ctx context.Context, url url.URL, user string, pass string, thumbprint *string, datacenter *string, computeResource *string) (*data.Data, error) {
+	d := data.Data{
+		Target: &common.Target{
+			URL:      &url,
+			User:     user,
+			Password: &pass,
+		},
+	}
+
+	if thumbprint != nil {
+		d.Thumbprint = *thumbprint
+	}
+
+	if datacenter != nil {
+		validator, err := validateTarget(ctx, &d)
+		if err != nil {
+			return nil, util.WrapError(500, err)
+		}
+
+		datacenterManagedObjectReference := types.ManagedObjectReference{Type: "Datacenter", Value: *datacenter}
+
+		datacenterObject, err := validator.Session.Finder.ObjectReference(ctx, datacenterManagedObjectReference)
+		if err != nil {
+			return nil, util.WrapError(500, err)
+		}
+
+		d.Target.URL.Path = datacenterObject.(*object.Datacenter).InventoryPath
+	}
+
+	if computeResource != nil {
+		d.ComputeResourcePath = *computeResource
+	}
+
+	return &d, nil
+}
+
+func validateTarget(ctx context.Context, d *data.Data) (*validate.Validator, error) {
+	if err := d.HasCredentials(); err != nil {
+		return nil, fmt.Errorf("Invalid Credentials: %s", err)
+	}
+
+	validator, err := validate.NewValidator(ctx, d)
+	if err != nil {
+		return nil, fmt.Errorf("Validation Error: %s", err)
+	}
+	// If dc is not set, and multiple datacenter is available, vic-machine ls will list VCHs under all datacenters.
+	validator.AllowEmptyDC()
+
+	_, err = validator.ValidateTarget(ctx, d)
+	if err != nil {
+		return nil, fmt.Errorf("Target validation failed: %s", err)
+	}
+	_, err = validator.ValidateCompute(ctx, d, false)
+	if err != nil {
+		return nil, fmt.Errorf("Compute resource validation failed: %s", err)
+	}
+
+	return validator, nil
+}
+
+// Copied from list.go, and appears to be present other places. TODO: deduplicate
+func upgradeStatusMessage(ctx context.Context, vch *vm.VirtualMachine, installerVer *version.Build, vchVer *version.Build) string {
+	if sameVer := installerVer.Equal(vchVer); sameVer {
+		return "Up to date"
+	}
+
+	upgrading, err := vch.VCHUpdateStatus(ctx)
+	if err != nil {
+		return fmt.Sprintf("Unknown: %s", err)
+	}
+	if upgrading {
+		return "Upgrade in progress"
+	}
+
+	canUpgrade, err := installerVer.IsNewer(vchVer)
+	if err != nil {
+		return fmt.Sprintf("Unknown: %s", err)
+	}
+	if canUpgrade {
+		return fmt.Sprintf("Upgradeable to %s", installerVer.ShortVersion())
+	}
+
+	oldInstaller, err := installerVer.IsOlder(vchVer)
+	if err != nil {
+		return fmt.Sprintf("Unknown: %s", err)
+	}
+	if oldInstaller {
+		return fmt.Sprintf("VCH has newer version")
+	}
+
+	// can't get here
+	return "Invalid upgrade status"
+}
--- lib/apiservers/service/restapi/handlers/vch_list_get.go
+++ lib/apiservers/service/restapi/handlers/vch_list_get.go
@@ -23,13 +23,11 @@ import (
 	"github.com/docker/docker/opts"
 	"github.com/go-openapi/runtime/middleware"
 
-	"github.com/vmware/vic/cmd/vic-machine/common"
 	"github.com/vmware/vic/lib/apiservers/service/models"
 	"github.com/vmware/vic/lib/apiservers/service/restapi/handlers/util"
 	"github.com/vmware/vic/lib/apiservers/service/restapi/operations"
 	"github.com/vmware/vic/lib/install/data"
 	"github.com/vmware/vic/lib/install/management"
-	"github.com/vmware/vic/lib/install/validate"
 	"github.com/vmware/vic/pkg/version"
 	"github.com/vmware/vic/pkg/vsphere/vm"
 )
@@ -43,15 +41,18 @@ type VCHDatacenterListGet struct {
 }
 
 func (h *VCHListGet) Handle(params operations.GetTargetTargetVchParams, principal interface{}) middleware.Responder {
-	d := buildData(
+	d, err := buildData(params.HTTPRequest.Context(),
 		url.URL{Host: params.Target},
 		principal.(Credentials).user,
 		principal.(Credentials).pass,
 		params.Thumbprint,
 		nil,
 		params.ComputeResource)
+	if err != nil {
+		return operations.NewGetTargetTargetVchDefault(util.StatusCode(err)).WithPayload(&models.Error{Message: err.Error()})
+	}
 
-	vchs, err := handle(params.HTTPRequest.Context(), d)
+	vchs, err := listVCHs(params.HTTPRequest.Context(), d)
 	if err != nil {
 		return operations.NewGetTargetTargetVchDefault(util.StatusCode(err)).WithPayload(&models.Error{Message: err.Error()})
 	}
@@ -60,15 +61,18 @@ func (h *VCHListGet) Handle(params operations.GetTargetTargetVchParams, principa
 }
 
 func (h *VCHDatacenterListGet) Handle(params operations.GetTargetTargetDatacenterDatacenterVchParams, principal interface{}) middleware.Responder {
-	d := buildData(
+	d, err := buildData(params.HTTPRequest.Context(),
 		url.URL{Host: params.Target},
 		principal.(Credentials).user,
 		principal.(Credentials).pass,
 		params.Thumbprint,
 		&params.Datacenter,
 		params.ComputeResource)
+	if err != nil {
+		return operations.NewGetTargetTargetVchDefault(util.StatusCode(err)).WithPayload(&models.Error{Message: err.Error()})
+	}
 
-	vchs, err := handle(params.HTTPRequest.Context(), d)
+	vchs, err := listVCHs(params.HTTPRequest.Context(), d)
 	if err != nil {
 		return operations.NewGetTargetTargetVchDefault(util.StatusCode(err)).WithPayload(&models.Error{Message: err.Error()})
 	}
@@ -76,7 +80,7 @@ func (h *VCHDatacenterListGet) Handle(params operations.GetTargetTargetDatacente
 	return operations.NewGetTargetTargetVchOK().WithPayload(operations.GetTargetTargetVchOKBody{Vchs: vchs})
 }
 
-func handle(ctx context.Context, d *data.Data) ([]*models.VCHListItem, error) {
+func listVCHs(ctx context.Context, d *data.Data) ([]*models.VCHListItem, error) {
 	validator, err := validateTarget(ctx, d)
 	if err != nil {
 		return nil, util.WrapError(400, err)
@@ -89,89 +93,6 @@ func handle(ctx context.Context, d *data.Data) ([]*models.VCHListItem, error) {
 	}
 
 	return vchsToModels(ctx, vchs, executor), nil
-}
-
-func buildData(url url.URL, user string, pass string, thumbprint *string, datacenter *string, computeResource *string) *data.Data {
-	d := data.Data{
-		Target: &common.Target{
-			URL:      &url,
-			User:     user,
-			Password: &pass,
-		},
-	}
-
-	if datacenter != nil {
-		// TODO: Convert ID to Name or update underlying code to accept ID
-		d.Target.URL.Path = *datacenter
-	}
-
-	if thumbprint != nil {
-		d.Thumbprint = *thumbprint
-	}
-
-	if computeResource != nil {
-		d.ComputeResourcePath = *computeResource
-	}
-
-	return &d
-}
-
-func validateTarget(ctx context.Context, d *data.Data) (*validate.Validator, error) {
-	if err := d.HasCredentials(); err != nil {
-		return nil, fmt.Errorf("Invalid Credentials: %s", err)
-	}
-
-	validator, err := validate.NewValidator(ctx, d)
-	if err != nil {
-		return nil, fmt.Errorf("Validation Error: %s", err)
-	}
-	// If dc is not set, and multiple datacenter is available, vic-machine ls will list VCHs under all datacenters.
-	validator.AllowEmptyDC()
-
-	_, err = validator.ValidateTarget(ctx, d)
-	if err != nil {
-		return nil, fmt.Errorf("Target validation failed: %s", err)
-	}
-	_, err = validator.ValidateCompute(ctx, d, false)
-	if err != nil {
-		return nil, fmt.Errorf("Compute resource validation failed: %s", err)
-	}
-
-	return validator, nil
-}
-
-// Copied from list.go, and appears to be present other places. TODO: deduplicate
-func upgradeStatusMessage(ctx context.Context, vch *vm.VirtualMachine, installerVer *version.Build, vchVer *version.Build) string {
-	if sameVer := installerVer.Equal(vchVer); sameVer {
-		return "Up to date"
-	}
-
-	upgrading, err := vch.VCHUpdateStatus(ctx)
-	if err != nil {
-		return fmt.Sprintf("Unknown: %s", err)
-	}
-	if upgrading {
-		return "Upgrade in progress"
-	}
-
-	canUpgrade, err := installerVer.IsNewer(vchVer)
-	if err != nil {
-		return fmt.Sprintf("Unknown: %s", err)
-	}
-	if canUpgrade {
-		return fmt.Sprintf("Upgradeable to %s", installerVer.ShortVersion())
-	}
-
-	oldInstaller, err := installerVer.IsOlder(vchVer)
-	if err != nil {
-		return fmt.Sprintf("Unknown: %s", err)
-	}
-	if oldInstaller {
-		return fmt.Sprintf("VCH has newer version")
-	}
-
-	// can't get here
-	return "Invalid upgrade status"
 }
 
 func vchsToModels(ctx context.Context, vchs []*vm.VirtualMachine, executor *management.Dispatcher) []*models.VCHListItem {
