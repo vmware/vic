6145374a6755045e1ab19e315b3c042b5effc061
Ian Whitlock <iwhitlock@vmware.com>
2017-10-18 16:02:15 -0500

    Refactor dynamic-vars.py to save global images list

--- tests/resources/dynamic-vars.py
+++ tests/resources/dynamic-vars.py
@@ -2,9 +2,8 @@ import os
 
 IS_LOCAL = False if (os.environ.has_key("DRONE_BUILD_NUMBER") and (int(os.environ['DRONE_BUILD_NUMBER']) != 0)) else True
 
-BUSYBOX =  'busybox' if IS_LOCAL else 'harbor.ci.drone.local/library/busybox'
-ALPINE =  'alpine' if IS_LOCAL else 'harbor.ci.drone.local/library/alpine'
-NGINX =  'nginx' if IS_LOCAL else 'harbor.ci.drone.local/library/nginx'
-DEBIAN =  'debian' if IS_LOCAL else 'harbor.ci.drone.local/library/debian'
-UBUNTU =  'ubuntu' if IS_LOCAL else 'harbor.ci.drone.local/library/ubuntu'
-REDIS =  'redis' if IS_LOCAL else 'harbor.ci.drone.local/library/redis'
+images = ['busybox', 'alpine', 'nginx','debian', 'ubuntu', 'redis']
+
+for image in images:
+    name = image if IS_LOCAL else 'harbor.ci.drone.local/library/{}'.format(image)
+    exec("{} = '{}'".format(image.upper(), name))
