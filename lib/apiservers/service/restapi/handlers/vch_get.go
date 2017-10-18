e5188936def526ea0eb4a3aa2156982c915dc7ad
angiecris <angiew@vmware.com>
2017-10-18 11:54:04 -0700

    syslog addr added to inspect output

--- lib/apiservers/service/restapi/handlers/vch_get.go
+++ lib/apiservers/service/restapi/handlers/vch_get.go
@@ -20,6 +20,7 @@ import (
 	"encoding/pem"
 	"fmt"
 	"net"
+	"net/http"
 	"net/url"
 	"strings"
 
@@ -98,23 +99,23 @@ func (h *VCHDatacenterGet) Handle(params operations.GetTargetTargetDatacenterDat
 func getVCH(ctx context.Context, d *data.Data) (*models.VCH, error) {
 	validator, err := validateTarget(ctx, d)
 	if err != nil {
-		return nil, util.WrapError(400, err)
+		return nil, util.WrapError(http.StatusBadRequest, err)
 	}
 
 	executor := management.NewDispatcher(validator.Context, validator.Session, nil, false)
 	vch, err := executor.NewVCHFromID(d.ID)
 	if err != nil {
-		return nil, util.NewError(500, fmt.Sprintf("Failed to inspect VCH: %s", err))
+		return nil, util.NewError(http.StatusInternalServerError, fmt.Sprintf("Failed to inspect VCH: %s", err))
 	}
 
 	err = validate.SetDataFromVM(validator.Context, validator.Session.Finder, vch, d)
 	if err != nil {
-		return nil, util.NewError(500, fmt.Sprintf("Failed to load VCH data: %s", err))
+		return nil, util.NewError(http.StatusInternalServerError, fmt.Sprintf("Failed to load VCH data: %s", err))
 	}
 
 	model, err := vchToModel(vch, d, executor)
 	if err != nil {
-		return nil, util.WrapError(500, err)
+		return nil, util.WrapError(http.StatusInternalServerError, err)
 	}
 
 	return model, nil
@@ -258,6 +259,11 @@ func vchToModel(vch *vm.VirtualMachine, d *data.Data, executor *management.Dispa
 			model.Runtime.DockerHost = fmt.Sprintf("%s:%s", publicIP, dockerPort)
 			model.Runtime.AdminPortal = fmt.Sprintf("https://%s:2378", publicIP)
 		}
+	}
+
+	// syslog_addr: syslog server address
+	if syslogConfig := vchConfig.Diagnostics.SysLogConfig; syslogConfig != nil {
+		model.SyslogAddr = syslogConfig.Network + "://" + syslogConfig.RAddr
 	}
 
 	return model, nil
