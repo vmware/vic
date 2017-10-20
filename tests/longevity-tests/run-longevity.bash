c63f30bf8a224d4e5783fec9209aef7d7c5ca816
Ian Whitlock <iwhitlock@vmware.com>
2017-10-20 14:19:45 -0500

    Add longevity testing scripts

---
+++ tests/longevity-tests/run-longevity.bash
@@ -0,0 +1,94 @@
+#!/bin/bash
+pushd /home/$USER/longevity-tests
+set -e
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+    echo "Usage: $0 target-cluster optional-harbor-version"
+    exit 1
+fi
+
+if [[ $1 != "6.0" && $1 != "6.5" ]]; then
+    echo "Please specify a target cluster. One of: 6.0, 6.5"
+    exit 1
+fi
+
+if [[ ! $(grep dns /etc/docker/daemon.json) ]]; then
+    echo "NOTE: /etc/docker/daemon.json should contain
+{
+ \"dns\": [\"10.118.81.1\", \"10.16.188.210\"]
+}
+
+ in order for this script to function behind VMW's firewall.
+
+ If the file does not exist, create it & restart the docker daemon before
+ attempting to run this script
+"
+    exit 1
+fi
+
+target="$1"
+
+# set an output directory
+odir="/home/$USER/vic-longevity-test-output-$(date -Iminute | sed 's/:/_/g')"
+
+
+# set up harbor if necessary
+if [[ $(docker ps | grep harbor) == "" ]]; then
+    if [[ $2 != "" ]]; then
+        hversion=$2
+    else
+        hversion="1.2.0"
+        echo "No Harbor version specified. Using default $hversion"
+    fi
+    ./get-and-start-harbor.bash $hversion
+fi
+
+echo "Building container images...."
+docker build -q -t longevity-base -f Dockerfile.foundation .
+docker build -q -t tests-"$target" -f Dockerfile."${target}" .
+
+# get latest tests
+pushd /home/$USER
+if [ ! -e vic ]; then
+    git clone https://github.com/vmware/vic vic
+else
+    pushd vic
+    git checkout master && git pull
+    popd
+fi
+
+# remove old binaries
+pushd /home/$USER/vic
+rm -rf bin && mkdir bin
+
+pushd bin
+input=$(gsutil ls -l gs://vic-engine-builds/vic_* | grep -v TOTAL | sort -k2 -r | head -n1 | xargs | cut -d ' ' -f 3 | cut -d '/' -f 4)
+echo "Downloading VIC build $input..."
+wget https://storage.googleapis.com/vic-engine-builds/$input -qO - | tar xz
+mv vic/* .
+rmdir vic
+popd
+
+
+echo "Creating container..."
+testsContainer=$(docker create -it\
+                        -w /go/src/github.com/vmware/vic/ \
+                        -v "$odir":/tmp/ -e GOVC_URL="$ip" \
+                        tests-"$target" \
+                        bash -c \
+                        ". secrets && pybot -d /tmp/ /go/src/github.com/vmware/vic/tests/manual-test-cases/Group14-Longevity/14-1-Longevity.robot;\
+                 mv *-container-logs.zip /tmp/ 2>/dev/null; \
+                 mv VCH-*-vmware.log /tmp/ 2>/dev/null; \
+                 mv vic-machine.log /tmp/ 2>/dev/null; \
+                 mv index.html* /tmp/ 2>/dev/null; \
+                 mv VCH-* /tmp/ 2>/dev/null")
+
+echo "Copying code and binaries into container...."
+docker cp /home/$USER/vic $testsContainer:/go/src/github.com/vmware/
+
+echo "Running tests.."
+echo "Run docker attach $testsContainer to interact with the container or use docker logs -f to simply view test output as the tests run"
+docker start $testsContainer
+
+echo "Output can be found in $odir"
+popd
+popd
--- tests/resources/dynamic-vars.py
+++ tests/resources/dynamic-vars.py
@@ -1,9 +1,32 @@
 import os
+from enum import Enum
 
-IS_LOCAL = False if (os.environ.has_key("DRONE_BUILD_NUMBER") and (int(os.environ['DRONE_BUILD_NUMBER']) != 0)) else True
 
-images = ['busybox', 'alpine', 'nginx','debian', 'ubuntu', 'redis']
+class TestEnvironment(Enum):
+    LOCAL = 0
+    DRONE = 1
+    LONGEVITY = 2
 
+
+def getEnvironment():
+    if (os.environ.has_key("DRONE_BUILD_NUMBER") and (int(os.environ['DRONE_BUILD_NUMBER']) != 0)):
+        return TestEnvironment.DRONE
+    elif os.environ.has_key("LONGEVITY"):
+        return TestEnvironment.LONGEVITY
+    else:
+        return TestEnvironment.LOCAL
+
+def getName(image):
+    environment = getEnvironment()
+    if environment == TestEnvironment.DRONE:
+        return 'harbor.ci.drone.local/library/{}'.format(image)
+    elif environment == TestEnvironment.LONGEVITY:
+        return 'harbor.longevity/library/{}'.format(image)
+    else:
+        return image
+
+# this global variable (images) is used by the Longevity scripts. If you change this, change those!
+# and don't inline it!
+images = ['busybox:latest', 'tomcat:1.2.1', 'busybox', 'alpine', 'nginx','debian', 'ubuntu', 'redis']
 for image in images:
-    name = image if IS_LOCAL else 'harbor.ci.drone.local/library/{}'.format(image)
-    exec("{} = '{}'".format(image.upper(), name))
+    exec("{} = '{}'".format(image.upper().replace(':', '_').replace('.', '_'), getName(image)))
