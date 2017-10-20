360910bf494fc325d39fdb1a8caa339a33ecf581
Ian Whitlock <iwhitlock@vmware.com>
2017-10-20 14:45:29 -0500

    Use a dict as a switch instead of an if/else tree

--- tests/resources/dynamic-vars.py
+++ tests/resources/dynamic-vars.py
@@ -17,16 +17,12 @@ def getEnvironment():
         return TestEnvironment.LOCAL
 
 def getName(image):
-    environment = getEnvironment()
-    if environment == TestEnvironment.DRONE:
-        return 'harbor.ci.drone.local/library/{}'.format(image)
-    elif environment == TestEnvironment.LONGEVITY:
-        return 'harbor.longevity/library/{}'.format(image)
-    else:
-        return image
+    return {TestEnvironment.DRONE: 'harbor.ci.drone.local/library/{}'.format(image),
+            TestEnvironment.LONGEVITY: 'harbor.longevity/library/{}'.format(image),
+            TestEnvironment.LOCAL: image}[getEnvironment()]
 
 # this global variable (images) is used by the Longevity scripts. If you change this, change those!
 # and don't inline it!
-images = ['busybox:latest', 'tomcat:1.2.1', 'busybox', 'alpine', 'nginx','debian', 'ubuntu', 'redis']
+images = ['busybox', 'alpine', 'nginx','debian', 'ubuntu', 'redis']
 for image in images:
     exec("{} = '{}'".format(image.upper().replace(':', '_').replace('.', '_'), getName(image)))
