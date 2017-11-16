c4a75ad65bc712d0181cee9570307713906d62b5
Zach Shepherd <zjs@users.noreply.github.com>
2017-11-16 14:35:20 -0800

    Support operation-specific logger configuration (#6776)
    For some use cases (such as the API), it may be useful to multiplex
    all log messages for a given operation to an additional location.
    
    Introduce support for this functionality and related unit tests.
    
    Add support for the non-formatted log message variants.
    
    Additionally, fix a minor bug in the FromContext method: Previously,
    an argument which was already an operation would hit the first case,
    unnecessarily resulting in a new operation.

--- pkg/trace/operation_test.go
+++ pkg/trace/operation_test.go
@@ -25,6 +25,7 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestContextUnpack(t *testing.T) {
@@ -135,4 +136,171 @@ func TestSanity(t *testing.T) {
 	if !assert.Error(t, err) {
 		return
 	}
+}
+
+// MockHook is a testify mock that can be registered as a logrus hook
+type MockHook struct {
+	mock.Mock
+}
+
+// Levels indicates that the mock log hook supports all log levels
+func (m *MockHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+// Fire records that it has been called and returns an error if configured
+func (m *MockHook) Fire(entry *logrus.Entry) error {
+	args := m.Called(entry)
+
+	return args.Error(0)
+}
+
+// cases defines the set of messages we expect to see and the level we expect to see each at
+var cases = map[string]logrus.Level{
+	"DebugfMessage": logrus.DebugLevel,
+	"DebugMessage":  logrus.DebugLevel,
+	"InfofMessage":  logrus.InfoLevel,
+	"InfoMessage":   logrus.InfoLevel,
+	"WarnfMessage":  logrus.WarnLevel,
+	"WarnMessage":   logrus.WarnLevel,
+	"ErrorfMessage": logrus.ErrorLevel,
+	"ErrorMessage":  logrus.ErrorLevel,
+}
+
+// buildMatcher creates a testify MatchedBy function for the supplied operation
+func buildMatcher(op Operation, shouldContainOpID bool) func(entry *logrus.Entry) bool {
+	return func(entry *logrus.Entry) bool {
+		if shouldContainOpID && !strings.Contains(entry.Message, op.id) {
+			return false // Log message should have contained the operation id, but did not
+		}
+
+		if !shouldContainOpID && strings.Contains(entry.Message, op.id) {
+			return false // Log message should not have contained the operation id, but did
+		}
+
+		for message, level := range cases {
+			if entry.Level == level && strings.Contains(entry.Message, message) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+// TestLogging demonstrates that log messages are relayed from the Operation to the Logger global
+func TestLogging(t *testing.T) {
+	defer func(original *logrus.Logger) { Logger = original }(Logger)
+	Logger = logrus.New()
+
+	op := NewOperation(context.Background(), "TestOperation")
+
+	m := new(MockHook)
+	Logger.Hooks.Add(m)
+	Logger.Level = logrus.DebugLevel
+
+	m.On("Fire", mock.MatchedBy(buildMatcher(op, true))).Return(nil)
+
+	op.Debugf("DebugfMessage")
+	op.Debug("DebugMessage")
+	op.Infof("InfofMessage")
+	op.Info("InfoMessage")
+	op.Warnf("WarnfMessage")
+	op.Warn("WarnMessage")
+	op.Errorf("ErrorfMessage")
+	op.Error(fmt.Errorf("ErrorMessage"))
+
+	m.AssertExpectations(t)
+	m.AssertNumberOfCalls(t, "Fire", 8)
+}
+
+// TestLogMuxing verifies that an operation-specific Logger can be configured and that both it and
+// the global Logger receive messages when logging methods are called on Operation
+func TestLogMuxing(t *testing.T) {
+	defer func(original *logrus.Logger) { Logger = original }(Logger)
+	Logger = logrus.New()
+
+	op := NewOperation(context.Background(), "TestOperation")
+
+	gm := new(MockHook)
+	Logger.Hooks.Add(gm)
+	Logger.Level = logrus.DebugLevel
+
+	lm := new(MockHook)
+	op.Logger = logrus.New()
+	op.Logger.Hooks.Add(lm)
+	op.Logger.Level = logrus.DebugLevel
+
+	gm.On("Fire", mock.MatchedBy(buildMatcher(op, true))).Return(nil)
+	lm.On("Fire", mock.MatchedBy(buildMatcher(op, false))).Return(nil)
+
+	op.Debugf("DebugfMessage")
+	op.Debug("DebugMessage")
+	op.Infof("InfofMessage")
+	op.Info("InfoMessage")
+	op.Warnf("WarnfMessage")
+	op.Warn("WarnMessage")
+	op.Errorf("ErrorfMessage")
+	op.Error(fmt.Errorf("ErrorMessage"))
+
+	gm.AssertExpectations(t)
+	gm.AssertNumberOfCalls(t, "Fire", 8)
+
+	lm.AssertExpectations(t)
+	lm.AssertNumberOfCalls(t, "Fire", 8)
+}
+
+// TestLogIsolation verifies that an operation-specific Loggers are actually operation-specific
+func TestLogIsolation(t *testing.T) {
+	op1 := NewOperation(context.Background(), "TestOperation")
+	op2 := NewOperation(context.Background(), "TestOperation")
+
+	lm1 := new(MockHook)
+	op1.Logger = logrus.New()
+	op1.Logger.Hooks.Add(lm1)
+	op1.Logger.Level = logrus.DebugLevel
+
+	lm2 := new(MockHook)
+	op2.Logger = logrus.New()
+	op2.Logger.Hooks.Add(lm2)
+	op2.Logger.Level = logrus.DebugLevel
+
+	lm1.On("Fire", mock.MatchedBy(buildMatcher(op1, false))).Return(nil)
+
+	op1.Debugf("DebugfMessage")
+	op1.Info("InfoMessage")
+	op1.Warnf("WarnfMessage")
+	op1.Errorf("ErrorfMessage")
+	op1.Error(fmt.Errorf("ErrorMessage"))
+
+	lm1.AssertExpectations(t)
+	lm1.AssertNumberOfCalls(t, "Fire", 5)
+
+	lm2.AssertExpectations(t)
+	lm2.AssertNumberOfCalls(t, "Fire", 0)
+}
+
+// TestLogInheritance verifies that an operation-specific Loggers are inherited by children
+func TestLogInheritance(t *testing.T) {
+	op := NewOperation(context.Background(), "TestOperation")
+
+	lm := new(MockHook)
+	op.Logger = logrus.New()
+	op.Logger.Hooks.Add(lm)
+	op.Logger.Level = logrus.DebugLevel
+
+	c1, _ := WithCancel(&op, "CancelChild")
+	c2 := WithValue(&c1, "foo", "bar", "ValueChild")
+	c3 := FromOperation(c2, "NormalChild")
+	c4 := FromContext(c3, "(Should == c3)")
+
+	lm.On("Fire", mock.MatchedBy(buildMatcher(op, false))).Return(nil)
+
+	op.Debugf("DebugfMessage")
+	c1.Infof("InfofMessage")
+	c2.Warnf("WarnfMessage")
+	c3.Errorf("ErrorfMessage")
+	c4.Error("ErrorMessage")
+
+	lm.AssertExpectations(t)
+	lm.AssertNumberOfCalls(t, "Fire", 5)
 }
