71a17cd2c78f8a5c35784de21af8e404b4aeaed4
Mikkel Hagen <mhagen@vmware.com>
2017-12-19 11:53:17 -0600

    [skip ci] Move all jenkins logic into a trackable script and upload the test results to GCP

---
+++ tests/longevity-tests/jenkins-job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd ~/vic
+
+git clean -fd
+git fetch https://github.com/vmware/vic master
+git pull
+
+cp ~/secrets .
+tests/longevity-tests/run-longevity.bash 6.0
+id=`docker ps -lq`
+echo $id
+
+docker logs -f $id
+
+docker cp $id:/tmp $id
+tar -cvzf $id.tar.gz $id
+gsutil cp $id.tar.gz gs://vic-longevity-results/
+
+echo $id
+rc=`docker inspect --format='{{.State.ExitCode}}' $id`
+exit $rc
