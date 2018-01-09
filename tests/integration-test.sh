5d751daa0ab919f26f4d5ec81aed0f70cc87c871
angiecris <angiew@vmware.com>
2018-01-09 10:47:01 -0800

    remove junk dirs in the log zip

--- tests/integration-test.sh
+++ tests/integration-test.sh
@@ -47,7 +47,7 @@ rc="$?"
 timestamp=$(date +%s)
 outfile="integration_logs_"$DRONE_BUILD_NUMBER"_"$DRONE_COMMIT".zip"
 
-zip -9 $outfile output.xml log.html report.html package.list *container-logs.zip *.log /var/log/vic-machine-server/vic-machine-server.log 
+zip -9 -j $outfile output.xml log.html report.html package.list *container-logs.zip *.log /var/log/vic-machine-server/vic-machine-server.log 
 
 # GC credentials
 keyfile="/root/vic-ci-logs.key"
