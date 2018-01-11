494e37f3d4f7f622693b61c616a55a514a66d3de
Mikkel Hagen <mhagen@vmware.com>
2018-01-11 16:09:22 -0600

    [skip ci].

--- tests/longevity-tests/run-longevity.bash
+++ tests/longevity-tests/run-longevity.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright 2017 VMware, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License
-
-#!/bin/bash
 set -e
 
 while getopts ":s:h:" opt; do
