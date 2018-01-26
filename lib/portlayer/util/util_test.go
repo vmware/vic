1543985a89f60b6c1c72709c846963bccf6ae039
George Hicken <ghicken@vmware.com>
2018-01-26 12:56:43 -0600

    Update cnc tests to include rename

--- lib/portlayer/util/util_test.go
+++ lib/portlayer/util/util_test.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"github.com/vmware/vic/lib/config"
 	"github.com/vmware/vic/lib/constants"
 	"github.com/vmware/vic/lib/spec"
