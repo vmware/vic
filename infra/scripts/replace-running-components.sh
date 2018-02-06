21add8d750a849da91d948f7c993d75553041150
Ian Whitlock <iwhitlock@vmware.com>
2018-02-06 15:52:12 -0600

    Formatting and cleanup

--- infra/scripts/replace-running-components.sh
+++ infra/scripts/replace-running-components.sh
@@ -15,13 +15,14 @@
 
 set -e
 
+if [ -z "$PS1" ]; then
+    interactive=1
+else
+    interactive=0
+fi
+
 BASE_DIR=$(dirname $(readlink -f "$BASH_SOURCE"))
 VIC_DIR=$(dirname $(readlink -f $BASE_DIR/..))
-
-# Grab vSphere's thumbprint for calling vic-machine
-function get-thumbprint () {
-    govc about.cert | grep "SHA-1 Thumbprint" | awk '{print $NF}'
-}
 
 # Run the command given on the VCH instead of locally
 function on-vch() {
@@ -34,63 +35,133 @@ function replace-component() {
     on-vch mv /sbin/$1 /tmp/old-$1
     on-vch mv /tmp/$1 /sbin/$1
     on-vch chmod 755 /sbin/$1
-    pid=$(on-vch ps -e --format='pid,args' | grep $1 | grep -v grep | awk '{print $1}')
+    pid=$(on-vch ps -e --format='pid,args' \
+              | grep $1 | grep -v grep | awk '{print $1}')
     on-vch kill -9 $pid
     on-vch rm -f /tmp/old-$1
 }
 
+# Determines whether the human VCH name is adequate to continue
 function vch-name-is-ambiguous () {
-    [ $($VIC_DIR/bin/vic-machine-linux ls --target=$target --user=$username --password=$password --thumbprint=$(get-thumbprint)  | grep $VIC_NAME | wc -l) -ne 1 ] && return 0 || return 1
+    [ $($VIC_DIR/bin/vic-machine-linux \
+            ls \
+            --target=$target \
+            --user=$username \
+            --password=$password \
+            --thumbprint=$(govc about.cert -k thumbprint) \
+            | grep $VIC_NAME | wc -l) -ne 1 ] \
+        && return 0 || return 1
 }
 
-# Check GOVC vars
-if [[ ! $(govc ls 2>/dev/null) ]]; then
-    echo "ERROR:"
-    echo "GOVC environment variables are required to use this command. Set the necessary variables to allow govc to connect to your vSphere deployment:";
-    echo "GOVC_USERNAME: username on vSphere target"
-    echo "GOVC_PASSWORD: password on vSphere target"
-    echo "GOVC_URL: IP or FQDN of your vSphere target"
-    echo "GOVC_INSECURE: set to 1 to disable tls verify when using govc to talk to vSphere"
-    exit 1
-fi
+# Check GOVC vars & print help text if not
+function check-govc-vars () {
+    if [[ ! $(govc ls 2>/dev/null) ]]; then
+        echo "ERROR:"
+        echo "GOVC environment variables are required to use this command. Set the necessary variables to allow govc to connect to your vSphere deployment:";
+        echo "GOVC_USERNAME: username on vSphere target"
+        echo "GOVC_PASSWORD: password on vSphere target"
+        echo "GOVC_URL: IP or FQDN of your vSphere target"
+        echo "GOVC_INSECURE: set to 1 to disable tls verify when using govc to talk to vSphere"
+        exit 1
+    fi
 
-# Check for our one required argument
-if [[ ! -v VIC_NAME ]] && [[ ! -v VIC_ID ]]; then
-    echo "Please set one of the following environment variables to specify the VCH which you would like to reconfigure:"
-    echo "VIC_NAME: name of VCH; matches --name argument for vic-machine"
-    echo "VIC_ID: ID of VCH, as displayed in output of vic-machine ls"
-    exit 1
-fi
+}
 
-username=$(govc env | grep GOVC_USERNAME | cut -d= -f2)
-password=$(govc env | grep GOVC_PASSWORD | cut -d= -f2)
-target=$(govc env | grep GOVC_URL | cut -d= -f2)
+# Make sure we have at least one of VIC_NAME or VIC_ID
+function check-vch-name-or-id () {
+    if [[ ! -v VIC_NAME ]] && [[ ! -v VIC_ID ]]; then
+        if [[ $interactive -eq 0 ]]; then
+            echo "Please set one of the following environment variables to specify the VCH which you would like to reconfigure:"
+            echo "VIC_NAME: name of VCH; matches --name argument for vic-machine"
+            echo "VIC_ID: ID of VCH, as displayed in output of vic-machine ls"
+            exit 1
+        fi
+        read -p "Enter VCH name: " VIC_NAME
+    fi
+}
 
+# Falls back on VIC_ID if possible, bails if not, assumes the VCH name is ambiguous
+function check-name-isnt-ambiguous ()  {
+    username=$(govc env | grep GOVC_USERNAME | cut -d= -f2)
+    password=$(govc env | grep GOVC_PASSWORD | cut -d= -f2)
+    target=$(govc env | grep GOVC_URL | cut -d= -f2)
 
-if [[ ! -v VIC_ID ]] && [[ $(vch-name-is-ambiguous) ]]; then
-    echo "The provided VIC name is ambiguous; please choose the correct VCH ID from the output below and assign it to the environment variable VIC_ID, e.g., export VIC_ID=12"
-    $VIC_DIR/bin/vic-machine-linux ls --target $GOVC_URL --user $GOVC_USER --password=$GOVC_PASSWORD --thumbprint=$(get-thumbprint)
-    exit 1
-fi
+    if [[ ! -v VIC_ID ]] && [[ $(vch-name-is-ambiguous) ]]; then
+        echo "The provided VIC name is ambiguous; please choose the correct VCH ID from the output below and assign it to the environment variable VIC_ID, e.g., export VIC_ID=12"
 
-if [[ ! -v VIC_ID ]]; then
-    VIC_ID=$($VIC_DIR/bin/vic-machine-linux ls --target=$target --user=$username --password=$password --thumbprint=$(get-thumbprint) | grep $VIC_NAME | awk '{print $1}')
-fi
+        $VIC_DIR/bin/vic-machine-linux\
+            ls \
+            --target $GOVC_URL \
+            --user $GOVC_USER \
+            --password=$GOVC_PASSWORD \
+            --thumbprint=$(govc about.cert -k thumbprint)
 
-$VIC_DIR/bin/vic-machine-linux debug --target=$target --id=$VIC_ID --user=$username --password=$password --thumbprint=$(get-thumbprint)
+        if [[ $interactive -eq 0 ]]; then
+            exit 1
+        fi
+        read -p "Enter unique VIC ID from above: " VIC_ID
+    fi
+}
 
-VCH_IP="$(govc vm.ip $VIC_NAME)"
+# Translates VCH name to ID if necessary
+function get-vic-id () {
+    if [[ ! -v VIC_ID ]]; then
+        VIC_ID=$(\
+                 $VIC_DIR/bin/vic-machine-linux\
+                     ls \
+                     --target=$target \
+                     --user=$username \
+                     --password=$password \
+                     --thumbprint=$(govc about.cert -k thumbprint)\
+                     | grep $VIC_NAME | awk '{print $1}')
+    fi
+}
 
+function get-ssh-keys() {
+    if [[ -z $VIC_KEY ]]; then
+        echo "Variable VIC_KEY not set. Provide the path to your public SSH key below."
+        if [[ $interactive -eq 0 ]]; then
+            exit 1
+        fi
+        read -p "Path to your public SSH key for access to VCH [/home/$USER/.ssh/id_rsa.pub]: " VIC_KEY
+    fi
+}
 
-if [[ $1 == "" ]]; then # replace everything
-    for x in port-layer-server docker-engine-server vicadmin vic-init; do
-        replace-component $x
-    done
-else
-    for x in $@; do # replace provided list
-        replace-component $x
-    done
-fi
+# Checks environment for required inputs
+function sanity-checks () {
+    check-govc-vars
+    check-vch-name-or-id
+    check-name-isnt-ambiguous
+    get-vic-id
+    get-ssh-keys
+}
 
+function enable-debug () {
+    $VIC_DIR/bin/vic-machine-linux debug \
+                                   --target=$target \
+                                   --id=$VIC_ID \
+                                   --user=$username \
+                                   --password=$password \
+                                   --thumbprint=$(govc about.cert -k thumbprint)
+}
+
+function replace-components () {
+    VCH_IP="$(govc vm.ip $VIC_NAME)"
+    if [[ $1 == "" ]]; then # replace everything
+        for x in port-layer-server docker-engine-server vicadmin vic-init; do
+            replace-component $x
+        done
+    else
+        for x in $@; do # replace provided list
+            replace-component $x
+        done
+    fi
+}
+
+sanity-checks
+enable-debug
+replace-components $@
+
+# fix restart?
 on-vch vic-init &
 echo "IP address may change when appliance finishes re-initializing. Get the new IP with govc vm.ip $VIC_NAME"
