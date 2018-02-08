9b971019af6951142c0842b091852653748b24de
Mikkel Hagen <mhagen@vmware.com>
2018-02-08 13:43:50 -0600

    [skip ci].

--- infra/integration-image/git-clone.sh
+++ infra/integration-image/git-clone.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sed -i 's|remotes/origin/*|remotes/origin/*\n\tfetch = +refs/pull/*/head:refs/remotes/origin/pr/|g' .git/config
-git fetch origin && git checkout pr/\${DRONE_PULL_REQUEST}
+git fetch origin && git checkout pr/$DRONE_PULL_REQUEST
