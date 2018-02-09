0dfee24adf76bdb7485392a7444a08bb4fa45c44
George Hicken <hickeng@users.noreply.github.com>
2018-02-09 12:57:34 -0800

    Improve comment

--- lib/install/management/store_files.go
+++ lib/install/management/store_files.go
@@ -159,8 +159,8 @@ func (d *Dispatcher) deleteFilesIteratively(m *object.DatastoreFileManager, ds *
 	defer trace.End(trace.Begin(dsPath, d.op))
 
 	if d.isVSAN(ds) {
-		// Get sorted result to make sure children files listed ahead of folder. Then we can empty folder before delete it
-		// This function specifically designed for vSan, as vSan sometimes will throw error to delete folder is the folder is not empty
+		// Get sorted result to make sure child files are listed ahead of their parent folder so we empty the folder before deleting it.
+		// This behaviour is specifically for vSan, as vSan sometimes throws an error when deleting a folder that is not empty.
 		res, err := d.getSortedChildren(ds, dsPath)
 		if err != nil {
 			if !types.IsFileNotFound(err) {
