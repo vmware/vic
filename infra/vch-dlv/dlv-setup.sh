e5b0f627cc395a6352ab4b569fce9332c10b6458
lcastellano <lcastellano@vmware.com>
2018-03-01 17:24:36 -0800

    Added no strict host checking (#7436)

--- infra/vch-dlv/dlv-setup.sh
+++ infra/vch-dlv/dlv-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -x
+SSH="ssh -o StrictHostKeyChecking=no"
+SCP="scp -o StrictHostKeyChecking=no"
 
 function usage() {
     echo "Usage: $0 -h vch-address -p password -s" >&2
@@ -48,21 +50,21 @@ fi
 
 # copy dlv binary
 if [ -f ${DLV_BIN} ]; then
-    sshpass -e scp ${DLV_BIN} root@${VCH_HOST}:/usr/local/bin
+    sshpass -e ${SCP} ${DLV_BIN} root@${VCH_HOST}:/usr/local/bin
 else
     echo $DLV_BIN does not exist. Run \"go get github.com/derekparker/delve/cmd/dlv\"
     exit 1
 fi
 
 # copy GOROOT env
-sshpass -e ssh root@${VCH_HOST} "mkdir -p /usr/local/go"
-sshpass -e scp -r ${GOROOT}/bin root@${VCH_HOST}:/usr/local/go
-sshpass -e scp -r ${GOROOT}/api root@${VCH_HOST}:/usr/local/go
-sshpass -e scp ${GOROOT}/VERSION root@${VCH_HOST}:/usr/local/go
-sshpass -e ssh root@${VCH_HOST} "ln -f -s /usr/local/go/bin/go /usr/local/bin/go"
+sshpass -e ${SSH} root@${VCH_HOST} "mkdir -p /usr/local/go"
+sshpass -e ${SCP} -r ${GOROOT}/bin root@${VCH_HOST}:/usr/local/go
+sshpass -e ${SCP} -r ${GOROOT}/api root@${VCH_HOST}:/usr/local/go
+sshpass -e ${SCP} ${GOROOT}/VERSION root@${VCH_HOST}:/usr/local/go
+sshpass -e ${SSH} root@${VCH_HOST} "ln -f -s /usr/local/go/bin/go /usr/local/bin/go"
 
 # open IPTABLES
-sshpass -e ssh root@${VCH_HOST} "iptables -A INPUT -p tcp -m tcp --dport 2345:2349 -j ACCEPT"
+sshpass -e ${SSH} root@${VCH_HOST} "iptables -A INPUT -p tcp -m tcp --dport 2345:2349 -j ACCEPT"
 
 # write remote dlv attach script
 TEMPFILE=$(mktemp)
@@ -91,7 +93,7 @@ fi
 dlv attach \${PID} --api-version 2 --headless --listen=:\${PORT} 
 EOF
 
-sshpass -e scp ${TEMPFILE} root@${VCH_HOST}:/usr/local/bin/dlv-attach-headless.sh
+sshpass -e ${SCP} ${TEMPFILE} root@${VCH_HOST}:/usr/local/bin/dlv-attach-headless.sh
 
 # write dlv detach script
 cat > ${TEMPFILE} <<EOF
@@ -119,17 +121,17 @@ fi
 kill \${PID}
 EOF
 
-sshpass -e scp ${TEMPFILE} root@${VCH_HOST}:/usr/local/bin/dlv-detach-headless.sh
+sshpass -e ${SCP} ${TEMPFILE} root@${VCH_HOST}:/usr/local/bin/dlv-detach-headless.sh
 
-sshpass -e ssh  root@${VCH_HOST} 'chmod +x /usr/local/bin/*'
+sshpass -e ${SSH}  root@${VCH_HOST} 'chmod +x /usr/local/bin/*'
 
 rm ${TEMPFILE}
 
 if [ -n "${COPY_SSH_KEY}" ]; then 
     # setup authorized_keys
-    sshpass -e ssh root@${VCH_HOST} "mkdir -p .ssh"
-    sshpass -e scp ${HOME}/.ssh/*.pub root@${VCH_HOST}:.ssh
-    sshpass -e ssh root@${VCH_HOST} 'cat ~/.ssh/*.pub > ~/.ssh/authorized_keys'
-    sshpass -e ssh root@${VCH_HOST} 'rm ~/.ssh/*.pub'
-    sshpass -e ssh root@${VCH_HOST} 'chmod 600 ~/.ssh/authorized_keys'
+    sshpass -e ${SSH} root@${VCH_HOST} "mkdir -p .ssh"
+    sshpass -e ${SCP} ${HOME}/.ssh/*.pub root@${VCH_HOST}:.ssh
+    sshpass -e ${SSH} root@${VCH_HOST} 'cat ~/.ssh/*.pub > ~/.ssh/authorized_keys'
+    sshpass -e ${SSH} root@${VCH_HOST} 'rm ~/.ssh/*.pub'
+    sshpass -e ${SSH} root@${VCH_HOST} 'chmod 600 ~/.ssh/authorized_keys'
 fi
