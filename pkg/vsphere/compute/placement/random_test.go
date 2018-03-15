638ccab590a6b6f0a1d482a51b88600ca581a888
Zach Tucker <jzt@users.noreply.github.com>
2018-03-15 13:02:53 -0500

    Implement RankedHostPolicy and host filtering  (#7459)

--- pkg/vsphere/compute/placement/random_test.go
+++ pkg/vsphere/compute/placement/random_test.go
@@ -16,42 +16,19 @@ package placement
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/vmware/govmomi/simulator"
-	"github.com/vmware/govmomi/vim25/types"
 	"github.com/vmware/vic/pkg/trace"
-	"github.com/vmware/vic/pkg/vsphere/extraconfig"
-	"github.com/vmware/vic/pkg/vsphere/session"
-	"github.com/vmware/vic/pkg/vsphere/tasks"
 	"github.com/vmware/vic/pkg/vsphere/test"
 	"github.com/vmware/vic/pkg/vsphere/vm"
 )
 
-// vpxModelSetup creates a VPX model, starts its server and populates the session. The caller must
-// clean up the model and the server once it is done using them.
-func vpxModelSetup(ctx context.Context, t *testing.T) (*simulator.Model, *simulator.Server, *session.Session) {
-	model := simulator.VPX()
-	if err := model.Create(); err != nil {
-		t.Fatal(err)
-	}
-
-	server := model.Service.NewServer()
-	sess, err := test.SessionWithVPX(ctx, server.URL.String())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return model, server, sess
-}
-
 func TestRandomRecommendHost(t *testing.T) {
 	op := trace.NewOperation(context.Background(), "TestRandomRecommendHost")
 
-	model, server, sess := vpxModelSetup(op, t)
+	model, server, sess := test.VpxModelSetup(op, t)
 	defer func() {
 		model.Remove()
 		server.Close()
@@ -62,47 +39,36 @@ func TestRandomRecommendHost(t *testing.T) {
 	hosts, err := cls.Hosts(op)
 	assert.NoError(t, err)
 
-	name := "test-vm"
-	vmx := fmt.Sprintf("%s/%s.vmx", name, name)
-	ds := sess.Datastore
-	secretKey, err := extraconfig.NewSecretKey()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	spec := types.VirtualMachineConfigSpec{
-		Name:    name,
-		GuestId: string(types.VirtualMachineGuestOsIdentifierOtherGuest),
-		Files: &types.VirtualMachineFileInfo{
-			VmPathName: fmt.Sprintf("[%s] %s", ds.Name(), vmx),
-		},
-		ExtraConfig: []types.BaseOptionValue{
-			&types.OptionValue{
-				Key:   extraconfig.GuestInfoSecretKey,
-				Value: secretKey.String(),
-			},
-		},
-	}
-
-	res, err := tasks.WaitForResult(op, func(op context.Context) (tasks.Task, error) {
-		return sess.VMFolder.CreateVM(op, spec, sess.Pool, nil)
-	})
+	moref, err := test.CreateVM(op, sess, "test-vm")
 	assert.NoError(t, err)
 
-	v := vm.NewVirtualMachine(op, sess, res.Result.(types.ManagedObjectReference))
+	v := vm.NewVirtualMachine(op, sess, moref)
 
-	rhp := NewRandomHostPolicy()
-	assert.False(t, rhp.CheckHost(op, v))
-	h, err := rhp.RecommendHost(op, v)
+	rhp, err := NewRandomHostPolicy(op, v.Cluster)
 	assert.NoError(t, err)
 
-	// TODO(jzt): come up with a better way to verify this than using a loop/moref comparison
+	assert.False(t, rhp.CheckHost(op, nil))
+	h, err := rhp.RecommendHost(op, nil)
+	assert.NoError(t, err)
+
+	top := h[0].Reference().String()
 	found := false
 	for _, host := range hosts {
-		if h.Reference().String() == host.Reference().String() {
+		if h[0].Reference().String() == host.Reference().String() {
 			found = true
+
+			// remove this host for the next test
+			h = h[1:]
 			break
 		}
 	}
 	assert.True(t, found)
+
+	// try with a subset
+	x, err := rhp.RecommendHost(op, h)
+	assert.NoError(t, err)
+	assert.Len(t, x, len(hosts)-1)
+	for _, host := range x {
+		assert.NotEqual(t, top, host.Reference().String())
+	}
 }
