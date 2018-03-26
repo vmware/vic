876a9e6c1ce68d61bebd250a9ed7178031a0db87
Loc Nguyen <nloc@vmware.com>
2018-03-26 14:18:38 -0700

    Use runc to run containers in the pod vm
    Instead of chroot, containers are now run using runc.  It is a not
    final.  We are currently using runc to generate a baseline config
    spec file but only override a few arguments in that json file.  We
    need to override more parameters from the pod spec.  That work
    still needs completing.
    
    Resolves #7217, #7244, #7246

--- isos/bootstrap.sh
+++ isos/bootstrap.sh
@@ -82,6 +82,7 @@ fi
 
 # copy in our components
 cp ${BIN}/tether-linux $(rootfs_dir $PKGDIR)/bin/tether
+cp /usr/sbin/runc $(rootfs_dir $PKGDIR)/bin/runc
 
 # kick off our components at boot time
 mkdir -p $(rootfs_dir $PKGDIR)/etc/systemd/system/vic.target.wants
