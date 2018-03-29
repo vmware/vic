fd015802959bd08fa118b0e98379d2bd6f86bbbf
Anchal Agrawal <anchala@vmware.com>
2018-03-29 15:54:09 -0500

    Grant ops-user minimum privileges on entities (#7587)
    This commit includes several changes to restrict the permissions
    granted to the vSphere operations user when the ops-grant-perms
    option is used in vic-machine create.
    
    Changes include:
    
      * The Endpoint role is also applied to the VCH inventory folder
        (only on vCenter). This allows us to reduce the privileges in
        the Datacenter role that is applied to the datacenter object.
    
      * Previously while granting the ops-user read-only access to the
        datacenter, we only checked if the ops-user was an Admin or in
        the Administrators group and didn't grant read-only access if so.
        This commit also adds a check to see if the ops-user already has
        enough permissions on the datacenter to satisfy the read-only role.
    
    Fixes #7053

--- lib/install/opsuser/opsuser_conf.go
+++ lib/install/opsuser/opsuser_conf.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 VMware, Inc. All Rights Reserved.
+// Copyright 2016-2018 VMware, Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -39,11 +39,6 @@ var RoleDataCenter = types.AuthorizationRole{
 	Privilege: []string{
 		"Datastore.Config",
 		"Datastore.FileManagement",
-		"VirtualMachine.Config.AddNewDisk",
-		"VirtualMachine.Config.AdvancedConfig",
-		"VirtualMachine.Config.RemoveDisk",
-		"VirtualMachine.Inventory.Create",
-		"VirtualMachine.Inventory.Delete",
 	},
 }
 
