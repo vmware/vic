1d1eec5bc0517efc66d5d1a8c2e3bb7f8b472ed3
Zach Shepherd <zjs@users.noreply.github.com>
2018-05-02 12:54:37 -0700

    validator: Remove ParseURL method (#7871)
    The ParseURL method was used only by code that tests it. Remove it.

--- lib/install/validate/validator.go
+++ lib/install/validate/validator.go
@@ -21,11 +21,10 @@ import (
 	"fmt"
 	"net/url"
 	"path"
-	"regexp"
 	"strings"
 	"time"
 
-	units "github.com/docker/go-units"
+	"github.com/docker/go-units"
 
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
@@ -174,30 +173,6 @@ func NewValidator(ctx context.Context, input *data.Data) (*Validator, error) {
 	}
 
 	return v, nil
-}
-
-var schemeMatch = regexp.MustCompile(`^\w+://`)
-
-// Starting from Go 1.8 the URL parser does not
-// work properly with URLs with no Scheme,
-// this function adds "https" as Scheme if necessary
-func ParseURL(s string) (*url.URL, error) {
-	var err error
-	var u *url.URL
-
-	if s != "" {
-		// Default the scheme to https
-		if !schemeMatch.MatchString(s) {
-			s = "https://" + s
-		}
-
-		u, err = url.Parse(s)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return u, nil
 }
 
 func (v *Validator) AllowEmptyDC() {
