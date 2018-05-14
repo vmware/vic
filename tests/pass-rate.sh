cc752ef8f681cbdef2083c43ee1a66a531deb4e2
Zach Shepherd <shepherdz@vmware.com>
2018-05-14 14:06:39 -0700

    Include pie chart when reporting CI passrate (#7950)
    To make the CI passrate more visible, include a pie chart summarizing
    successes, failures, and pending CI runs.
    
    Additionally, link to the repository instead of just displaying the
    name as text.

--- tests/pass-rate.sh
+++ tests/pass-rate.sh
@@ -19,21 +19,27 @@
 sprint_start=$(gdate -d "last wednesday-$(( ($(gdate +%W 2>/dev/null)+0)%2 )) weeks" "+%Y-%m-%dT%H:%M:%S" 2>/dev/null) || sprint_start=$(date -d "last wednesday-$(( ($(date +%W)+0)%2 )) weeks" "+%Y-%m-%dT%H:%M:%S")
 
 commits=$(curl -s https://api.github.com/repos/vmware/vic/commits?access_token=$GITHUB_AUTOMATION_API_KEY\&since=$sprint_start | jq -r ' map(.sha) | join(",")')
-curl -s https://api.github.com/repos/vmware/vic/statuses/{$commits}?access_token=$GITHUB_AUTOMATION_API_KEY | jq '.[] | select((.context == "continuous-integration/vic/push") and (.state != "pending")) | "\(.target_url): \(.state)"' | tee status.out
+curl -s https://api.github.com/repos/vmware/vic/status/{$commits}?access_token=$GITHUB_AUTOMATION_API_KEY | jq '.statuses[] | select(.context == "continuous-integration/vic/push") | "\(.target_url): \(.state)"' | tee status.out
 
 failures=$(cat status.out | grep -c failure)
 successes=$(cat status.out | grep -c success)
+pending=$(cat status.out | grep -c pending)
 
-let total=$successes+$failures
-if [ $total -eq 0 ]; then
+let complete=$successes+$failures
+if [ $complete -eq 0 ]; then
     # This should be "undefined", but starting at 100% seems reasonable given how this is used.
     passrate="100.00"
 else
-    passrate=$(bc -l <<< "scale=2;100 * ($successes / $total)")
+    passrate=$(bc -l <<< "scale=2;100 * ($successes / $complete)")
 fi
 
-echo "Number of failed merges to master in the $total merges since $sprint_start: $failures"
-echo "Number of successful merges to master in the $total merges since $sprint_start: $successes"
+echo "Number of failed runs on merges to master in the $complete completed builds since $sprint_start: $failures"
+echo "Number of successful runs on merges to master in the $complete completed builds since $sprint_start: $successes"
+echo "Number of runs since $sprint_start which are still pending: $pending"
 
-echo "Current vmware/vic CI passrate: $passrate"
-curl --max-time 10 --retry 3 -s -d "payload={'channel': '#vic-bots', 'text': 'Current vmware/vic CI passrate: $passrate%'}" "$SLACK_URL"
+echo "Current vmware/vic CI passrate: $passrate (of completed)"
+if [ $complete -eq 0 ]; then
+    curl --max-time 10 --retry 3 -s --data-urlencode "payload={'channel': '#vic-bots', 'text': 'Current <https://github.com/vmware/vic|vmware/vic> CI passrate: $passrate% (of $complete completed this sprint)'}" "$SLACK_URL"
+else
+    curl --max-time 10 --retry 3 -s --data-urlencode "payload={'channel': '#vic-bots', 'attachments':[{'text': 'Current <https://github.com/vmware/vic|vmware/vic> CI passrate: $passrate% (of $complete completed this sprint)', 'image_url': 'http://chart.googleapis.com/chart?cht=p&chs=250x150&chco=dc3912|109618|3366cc&chl=failure|success|pending&chd=t:$failures,$successes,$pending', 'footer':'pass-rate.sh'}]}" "$SLACK_URL"
+fi
