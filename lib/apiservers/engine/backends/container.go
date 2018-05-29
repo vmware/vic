8f45d075d63e94f60a4f4234ca5a9083cf1b8faf
matthewavery <mavery@vmware.com>
2018-05-29 14:47:33 -0700

    Correct state expectations for the TaskWaitHelper calls in ExecStart

--- lib/apiservers/engine/backends/container.go
+++ lib/apiservers/engine/backends/container.go
@@ -535,8 +535,8 @@ func (c *ContainerBackend) ContainerExecStart(ctx context.Context, eid string, s
 	}
 
 	go func() {
-		targetState := map[string]bool{constants.TaskRunningState: true}
-		waitState := map[string]bool{constants.TaskRunningState: true, constants.TaskUnknownState: true}
+		targetState := map[string]bool{constants.TaskRunningState: true, constants.TaskStoppedState: true}
+		waitState := map[string]bool{constants.TaskCreatedState: true, constants.TaskUnknownState: true}
 		_, err := c.taskStateWaitHelper(taskOp, id, eid, name, targetState, waitState)
 		if err != nil {
 			op.Errorf("Wait for exec start on %s.%s: %s", eid, id, err)
@@ -607,7 +607,7 @@ func (c *ContainerBackend) ContainerExecStart(ctx context.Context, eid string, s
 
 	op.Debugf("Waiting for completion: task(%s), container(%s)", eid, name)
 
-	targetState := map[string]bool{constants.TaskRunningState: true}
+	targetState := map[string]bool{constants.TaskStoppedState: true}
 	waitState := map[string]bool{constants.TaskRunningState: true, constants.TaskUnknownState: true}
 	_, err := c.taskStateWaitHelper(taskOp, id, eid, name, targetState, waitState)
 	op.Infof("task %s.%s has stopped: %s", err)
