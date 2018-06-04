b0e03e67e88478c4fac2b87f9dd37f3532aae8d5
Mikkel Hagen <mhagen@vmware.com>
2018-06-04 09:33:27 -0500

    [skip ci].

--- tests/integration-test.sh
+++ tests/integration-test.sh
@@ -44,7 +44,7 @@ if (echo $prBody | grep -q "\[shared datastore="); then
     export TEST_DATASTORE=$datastore
 fi
 
-jobs="2"
+jobs="3"
 if (echo $prBody | grep -q "\[parallel jobs="); then
     parallel=$(echo $prBody | grep "\[parallel jobs=")
     jobs=$(echo $parallel | awk -F"\[parallel jobs=" '{sub(/\].*/,"",$2);print $2}')
