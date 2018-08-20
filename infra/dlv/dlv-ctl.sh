c7d40ac878b09c577c307bc5331c7dd39619ed7c
lcastellano <lcastellano@vmware.com>
2018-08-20 09:24:46 -0700

    Integrated DLV support from vic master (#8117)

--- infra/dlv/dlv-ctl.sh
+++ infra/dlv/dlv-ctl.sh
@@ -13,14 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.!/bin/bash
 
+set -eo pipefail
+
 SSH="ssh -o StrictHostKeyChecking=no"
-SCP="scp -o StrictHostKeyChecking=no"
+SCP="scp -q -o StrictHostKeyChecking=no"
 
 REMOTE_DLV_ATTACH=/usr/local/bin/dlv-attach-headless.sh
 REMOTE_DLV_DETACH=/usr/local/bin/dlv-detach-headless.sh
 
 function usage() {
-    echo "Usage: $0 -h vch-address [-a/-d] -p password [attach/detach] target" >&2
+    echo "Usage: $0 -h vch-address [-a/-d] [attach/detach] target" >&2
     echo "Valid targets are: "
     echo "    vic-init"
     echo "    vic-admin"
@@ -31,17 +33,13 @@ function usage() {
     exit 1
 }
 
-while getopts "h:p:ad" flag
+while getopts "h:adl" flag
 do
     case $flag in
 
         h)
             # Optional
             export DLV_TARGET_HOST="$OPTARG"
-            ;;
-
-        p)
-            export SSHPASS="$OPTARG"
             ;;
 
         a)
@@ -52,6 +50,10 @@ do
             export COMMAND="detach"
             ;;
 
+        l)
+            export COMMAND="log"
+            ;;
+
         *)
             usage
             ;;
@@ -59,6 +61,14 @@ do
 done
 
 shift $((OPTIND-1))
+
+if [ -n "${DOCKER_HOST}" -a -z "${DLV_TARGET_HOST}" ]; then
+    export DLV_TARGET_HOST=$(echo $DOCKER_HOST | cut -d ':' -f 1)
+fi
+
+if [ -z "${DLV_TARGET_HOST}" ]; then
+    usage
+fi
 
 if [[ -z "${COMMAND}" &&  $# != 2 ]]; then
     usage
@@ -80,6 +90,8 @@ case ${TARGET} in
         ;;
 
     vic-admin)
+        # Change target to vicadmin
+        TARGET=vicadmin
         PORT=2346
         ;;
 
@@ -108,20 +120,12 @@ if [ -z "${DLV_TARGET_HOST}" ]; then
     usage
 fi
 
-if [ -n "${SSHPASS}" ]; then
-    SSH="sshpass -e ${SSH}"
-    SCP="sshpass -e ${SCP}"
-
-    if [ ! -f /usr/bin/sshpass ]; then
-        echo sshpass must be installed. Run \"apt-get install sshpass\"
-        exit 1
-    fi
-fi
-
 if [ ${COMMAND} == "attach" ]; then
-    ${SSH} root@${DLV_TARGET_HOST} "nohup /usr/local/bin/dlv-attach-headless.sh $TARGET $PORT >  /var/tmp/${TARGET}.log 2>&1 &"
+    ${SSH} root@${DLV_TARGET_HOST} "nohup /usr/local/bin/dlv-attach-headless.sh $TARGET $PORT > /var/tmp/${TARGET}.log 2>&1 &"
 elif [ ${COMMAND} == "detach" ]; then
     ${SSH} root@${DLV_TARGET_HOST} "/usr/local/bin/dlv-detach-headless.sh $PORT"
+elif [ ${COMMAND} == "log" ]; then
+    ${SSH} root@${DLV_TARGET_HOST} "cat /var/tmp/${TARGET}.log"
 else
     usage
 fi
