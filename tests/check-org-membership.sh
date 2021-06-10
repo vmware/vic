b6a09e704649de25f7b02e94b38a3e08816e7a92
wendingh <wendingh@vmware.com>
2021-06-10 03:13:24 -0700

    Put the github access token into request headers
    For more detail, please view the url:
    https://developer.github.com/changes/2020-02-10-deprecating-auth-through-query-param/

--- tests/check-org-membership.sh
+++ tests/check-org-membership.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2018 VMware, Inc. All Rights Reserved.
+# Copyright 2020-2021 VMware, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,12 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-set -e -o pipefail +h && [ -n "$DEBUG" ] && set -x
 
-prNumber=$(drone build info --format "{{ .Ref }}" vmware/vic "${DRONE_BUILD_NUMBER}" | cut -f 3 -d'/')
-prBody=$(curl -H "Authorization: token $GITHUB_AUTOMATION_API_KEY" "https://api.github.com/repos/vmware/vic/pulls/${prNumber}" | jq -r ".body")
+set -ex
 
-if ! (echo "${prBody}" | grep -q "\[skip unit\]"); then
-  make test
-  codecov --token "${CODECOV_TOKEN}" --file .cover/cover.out
+echo ${DRONE_COMMIT_AUTHOR}
+echo $SKIP_CHECK_MEMBERSHIP
+
+if [ "$SKIP_CHECK_MEMBERSHIP" == "true" ]; then
+  echo 'check-org-membership step skipped'
+  exit 0
+fi
+
+if [ ! $(curl --silent -H "Authorization: token $GITHUB_AUTOMATION_API_KEY" "https://api.github.com/orgs/vmware/members/${DRONE_COMMIT_AUTHOR}") ]; then
+  echo "checked origin membership successfully"
+else
+  echo "failed to check origin membership"
+  exit 1
 fi
